eefbeb952b27545ce2bc3b3a8b8bbbc9f6e6a8f5
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-09-13 14:07:54 +0400

    feat: [Tests] Update tests for inject params by attribute.

--- tests/Fixtures/Attributes/Lorem.php
+++ tests/Fixtures/Attributes/Lorem.php
@@ -12,4 +12,12 @@ class Lorem
         #[Inject]
         public SimpleDbInterface $simpleDb
     ) {}
+
+    public function doIt(
+        Logger $logger,
+        #[Inject(id: 'app.defaultName')]
+        string $userName
+    ): string {
+        return \sprintf('I log to [%s] with data [%s]', $logger->file, $this->simpleDb->insert($userName));
+    }
 }
