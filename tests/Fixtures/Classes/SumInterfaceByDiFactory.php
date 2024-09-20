8121ece5d43bef5155d3b583fd20068fd97c0d51
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-09-20 17:19:12 +0400

    dev [Tests]: Add class to tests fixture.

--- tests/Fixtures/Classes/SumInterfaceByDiFactory.php
+++ tests/Fixtures/Classes/SumInterfaceByDiFactory.php
@@ -8,7 +8,7 @@ use Kaspi\DiContainer\Interfaces\DiFactoryInterface;
 use Psr\Container\ContainerInterface;
 use Tests\Fixtures\Classes\Interfaces\SumInterface;
 
-class SumInterfaceFactory implements DiFactoryInterface
+class SumInterfaceByDiFactory implements DiFactoryInterface
 {
     public function __invoke(ContainerInterface $container): SumInterface
     {
