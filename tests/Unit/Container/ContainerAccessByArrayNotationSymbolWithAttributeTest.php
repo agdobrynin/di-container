59e1b4a15d8d4d562d6354d3c6c3189a2dd468b1
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-09-28 11:20:54 +0400

    dev: [Tests] Covers.

--- tests/Unit/Container/ContainerAccessByArrayNotationSymbolWithAttributeTest.php
+++ tests/Unit/Container/ContainerAccessByArrayNotationSymbolWithAttributeTest.php
@@ -12,10 +12,10 @@ use Tests\Fixtures;
 /**
  * @covers \Kaspi\DiContainer\Attributes\DiFactory
  * @covers \Kaspi\DiContainer\Attributes\Inject
+ * @covers \Kaspi\DiContainer\Attributes\Service
  * @covers \Kaspi\DiContainer\Autowired
  * @covers \Kaspi\DiContainer\DiContainer
  * @covers \Kaspi\DiContainer\DiContainerFactory
- * @covers \Kaspi\DiContainer\Attributes\Service
  *
  * @internal
  */
