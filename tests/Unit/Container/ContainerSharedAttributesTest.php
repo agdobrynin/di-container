e89c58864a0db03b209f154f30833513ad846167
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-10-03 18:55:11 +0400

    dev: [Tests] Update tests.

---
+++ tests/Unit/Container/ContainerSharedAttributesTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Container;
+
+use PHPUnit\Framework\TestCase;
+
+class ContainerSharedAttributesTest extends TestCase
+{
+
+}
---
+++ tests/Unit/Container/ContainerSharedDefinitionTest.php
@@ -0,0 +1,275 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Container;
+
+use Kaspi\DiContainer\Autowired;
+use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiContainerConfig;
+use Kaspi\DiContainer\DiContainerFactory;
+use Kaspi\DiContainer\Interfaces\DiContainerInterface;
+use PHPUnit\Framework\TestCase;
+use Tests\Fixtures\Classes\Db;
+use Tests\Fixtures\Classes\DbDiFactory;
+use Tests\Fixtures\Classes\FileCache;
+use Tests\Fixtures\Classes\Interfaces\SumInterface;
+use Tests\Fixtures\Classes\Sum;
+
+/**
+ * @covers \Kaspi\DiContainer\Attributes\DiFactory
+ * @covers \Kaspi\DiContainer\Attributes\Inject
+ * @covers \Kaspi\DiContainer\Autowired
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiContainerDefinition
+ * @covers \Kaspi\DiContainer\DiContainerFactory
+ *
+ * @internal
+ */
+class ContainerSharedDefinitionTest extends TestCase
+{
+    public function testContainerSharedDefinitionDefaultFromDiContainerFactory(): void
+    {
+        $c = (new DiContainerFactory())->make();
+
+        $this->assertNotSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionFalseByDefinition(): void
+    {
+        $c = (new DiContainerFactory())->make([
+            FileCache::class => [
+                DiContainerInterface::SHARED => false,
+            ],
+        ]);
+
+        $this->assertNotSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionTrueByDefinition(): void
+    {
+        $c = (new DiContainerFactory())->make([
+            FileCache::class => [
+                DiContainerInterface::SHARED => true,
+            ],
+        ]);
+
+        $this->assertSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionFalseByConfig(): void
+    {
+        $c = new DiContainer(
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+                isSharedServiceDefault: false
+            )
+        );
+
+        $this->assertNotSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionTrueByConfig(): void
+    {
+        $c = new DiContainer(
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+                isSharedServiceDefault: true
+            )
+        );
+
+        $this->assertSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionBySetMethodDefault(): void
+    {
+        $c = new DiContainer(
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $c->set(id: FileCache::class);
+
+        $this->assertNotSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionBySetMethodFalse(): void
+    {
+        $c = new DiContainer(
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $c->set(id: FileCache::class, shared: false);
+
+        $this->assertNotSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionBySetMethodTrue(): void
+    {
+        $c = new DiContainer(
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $c->set(id: FileCache::class, shared: true);
+
+        $this->assertSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionClosureTrue(): void
+    {
+        $definition = [
+            FileCache::class => [
+                static fn () => new FileCache(),
+                DiContainerInterface::SHARED => true,
+            ]
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionClosureFalse(): void
+    {
+        $definition = [
+            FileCache::class => [
+                static fn () => new FileCache(),
+                DiContainerInterface::SHARED => false,
+            ]
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertNotSame($c->get(FileCache::class), $c->get(FileCache::class));
+    }
+
+    public function testContainerSharedDefinitionDiFabricDefault(): void
+    {
+        $definition = [
+            Db::class => DbDiFactory::class,
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertEquals(['one', 'two'], $c->get(Db::class)->all());
+        $this->assertNotSame($c->get(Db::class), $c->get(Db::class));
+    }
+
+    public function testContainerSharedDefinitionDiFabricFalse(): void
+    {
+        $definition = [
+            Db::class => [
+                DbDiFactory::class,
+                DiContainerInterface::SHARED => false,
+            ],
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertEquals(['one', 'two'], $c->get(Db::class)->all());
+        $this->assertNotSame($c->get(Db::class), $c->get(Db::class));
+    }
+
+    public function testContainerSharedDefinitionDiFabricTrue(): void
+    {
+        $definition = [
+            Db::class => [
+                DbDiFactory::class,
+                DiContainerInterface::SHARED => true,
+            ],
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertEquals(['one', 'two'], $c->get(Db::class)->all());
+        $this->assertSame($c->get(Db::class), $c->get(Db::class));
+    }
+
+    public function testContainerSharedDefinitionInterfaceDefault(): void
+    {
+        $definition = [
+            SumInterface::class => Sum::class, // default service not shared defined in DiContainerConfig!
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertInstanceOf(Sum::class, $c->get(SumInterface::class));
+        $this->assertNotSame($c->get(SumInterface::class), $c->get(SumInterface::class));
+    }
+
+    public function testContainerSharedDefinitionInterfaceFalse(): void
+    {
+        $definition = [
+            SumInterface::class => [
+                Sum::class,
+                DiContainerInterface::SHARED => false // service not shared!
+            ],
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertInstanceOf(Sum::class, $c->get(SumInterface::class));
+        $this->assertNotSame($c->get(SumInterface::class), $c->get(SumInterface::class));
+    }
+
+    public function testContainerSharedDefinitionInterfaceTrue(): void
+    {
+        $definition = [
+            SumInterface::class => [
+                Sum::class,
+                DiContainerInterface::SHARED => true // service shared!
+            ],
+        ];
+
+        $c = new DiContainer(
+            definitions: $definition,
+            config: new DiContainerConfig(
+                autowire: new Autowired(),
+            )
+        );
+
+        $this->assertInstanceOf(Sum::class, $c->get(SumInterface::class));
+        $this->assertSame($c->get(SumInterface::class), $c->get(SumInterface::class));
+    }
+}
