85be35c5f010b35be5c5a2b82804e9ca6506b0eb
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-10-11 23:24:06 +0400

    dev: [Tests] Update test

--- tests/Fixtures/Attributes/ClassWithInjectByAttributeTowServicesOneType.php
+++ tests/Fixtures/Attributes/ClassWithInjectByAttributeTowServicesOneType.php
@@ -9,7 +9,7 @@ use Kaspi\DiContainer\Attributes\Inject;
 class ClassWithInjectByAttributeTowServicesOneType
 {
     public function __construct(
-        #[Inject(arguments: ['array' => ['one', 'tow']])]
+        #[Inject(arguments: ['array' => ['one', 'two']])]
         public \ArrayIterator $iterator1,
         #[Inject(arguments: ['array' => ['three', 'four']])]
         public \ArrayIterator $iterator2,
