a9fd552e99e5a317af1016789798083cbf997458
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-10-13 23:08:32 +0400

    refactor: [Attribute DiFactory] Throw exception AutowiredAttributeException if invalid value.

--- src/DiContainer/Attributes/DiFactory.php
+++ src/DiContainer/Attributes/DiFactory.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer\Attributes;
 
+use Kaspi\DiContainer\Exception\AutowiredAttributeException;
 use Kaspi\DiContainer\Interfaces\DiFactoryInterface;
 
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_PARAMETER)]
@@ -15,7 +16,7 @@ final class DiFactory
     public function __construct(public string $id, public array $arguments = [], public bool $isSingleton = false)
     {
         \is_a($this->id, DiFactoryInterface::class, true)
-            || throw new \InvalidArgumentException("Parameter '{$this->id}' must be implement '".DiFactoryInterface::class."' interface");
+            || throw new AutowiredAttributeException("Parameter '{$this->id}' must be implement '".DiFactoryInterface::class."' interface");
     }
 
     public static function makeFromReflection(\ReflectionClass|\ReflectionParameter $parameter): ?self
