d0ad39ee97f45a3506b1259bbb756eec84e23ffe
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-10-17 12:48:41 +0400

    feat: [diDefinition] Throw exception is diDefinition is empty.

--- src/DiContainer/function.php
+++ src/DiContainer/function.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer;
 
+use Kaspi\DiContainer\Exception\ContainerException;
 use Kaspi\DiContainer\Interfaces\DiContainerInterface;
 
 // @phan-suppress-next-line PhanUnreferencedFunction
 function diDefinition(mixed $definition = null, array $arguments = [], ?bool $isSingleton = null): array
 {
-    return ($definition ? [0 => $definition] : [])
+    return (
+        ($definition ? [0 => $definition] : [])
         + ($arguments ? [DiContainerInterface::ARGUMENTS => $arguments] : [])
-        + (null !== $isSingleton ? [DiContainerInterface::SINGLETON => $isSingleton] : []);
+        + (null !== $isSingleton ? [DiContainerInterface::SINGLETON => $isSingleton] : [])
+    ) ?: throw new ContainerException('Definition function is empty.');
 }
