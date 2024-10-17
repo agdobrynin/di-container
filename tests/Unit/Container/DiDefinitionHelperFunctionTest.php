3610e8bd16469db2a3456b3509a30d446735f561
Alex Dobrynin <ag.dobrynin@gmail.com>
2024-10-17 21:21:03 +0400

    dev: [Tests] Update Tests. (#51)

--- tests/Unit/Container/DiDefinitionHelperFunctionTest.php
+++ tests/Unit/Container/DiDefinitionHelperFunctionTest.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Container;
 
+use Kaspi\DiContainer\DiContainerFactory;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerExceptionInterface;
+use Tests\Fixtures\Classes\Interfaces\SumInterface;
+use Tests\Fixtures\Classes\Sum;
 
 use function Kaspi\DiContainer\diDefinition;
 
 /**
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiContainerDefinition
+ * @covers \Kaspi\DiContainer\DiContainerFactory
  * @covers \Kaspi\DiContainer\diDefinition
  *
  * @internal
@@ -86,6 +93,10 @@ class DiDefinitionHelperFunctionTest extends TestCase
         yield 'has id and arguments' => [
             self::class, null, ['name' => 'Ivan', 'service' => '@inject'], null, [self::class => ['arguments' => ['name' => 'Ivan', 'service' => '@inject']]],
         ];
+
+        yield 'has definition and arguments' => [
+            null, self::class, ['name' => 'Ivan', 'service' => '@inject'], null, [self::class, 'arguments' => ['name' => 'Ivan', 'service' => '@inject']],
+        ];
     }
 
     /**
@@ -94,5 +105,25 @@ class DiDefinitionHelperFunctionTest extends TestCase
     public function testSuccess(?string $containerKey, mixed $definition, ?array $arguments, ?bool $isSingleton, array $expect): void
     {
         $this->assertEquals($expect, diDefinition(containerKey: $containerKey, definition: $definition, arguments: $arguments, isSingleton: $isSingleton));
+    }
+
+    public function testFromReadme(): void
+    {
+        $d1 = diDefinition(
+            containerKey: SumInterface::class,
+            definition: Sum::class,
+            arguments: ['init' => 50]
+        );
+
+        $d2 = diDefinition(
+            containerKey: Sum::class,
+            arguments: ['init' => 10],
+            isSingleton: true
+        );
+
+        $c = (new DiContainerFactory())->make($d1 + $d2);
+
+        $this->assertEquals(-10, $c->get(Sum::class)->add(-20));
+        $this->assertEquals(20, $c->get(SumInterface::class)->add(-30));
     }
 }
