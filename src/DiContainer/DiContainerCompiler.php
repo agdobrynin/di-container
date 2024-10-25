84159994cc6290e33d5fc5ecdde50eada715e681
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-10-25 12:26:07 +0400

    feat: [DiContainerCompiler] WIP.

--- src/DiContainer/DiContainerCompiler.php
+++ src/DiContainer/DiContainerCompiler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer;
 
+use Kaspi\DiContainer\Exception\ContainerException;
 use Kaspi\DiContainer\Interfaces\DiContainerCallInterface;
 use Kaspi\DiContainer\Interfaces\DiContainerCompilerInterface;
 use Kaspi\DiContainer\Interfaces\DiContainerConfigInterface;
@@ -11,9 +12,21 @@ use Kaspi\DiContainer\Interfaces\DiContainerInterface;
 
 class DiContainerCompiler extends DiContainer implements DiContainerCompilerInterface
 {
+    protected bool $isCompiled = false;
+
     public function __construct(protected string $compiledContainerFile, iterable $definitions = [], ?DiContainerConfigInterface $config = null)
     {
         parent::__construct($definitions, $config);
+    }
+
+    public function set(string $id, mixed $definition = null, ?array $arguments = null, ?bool $isSingleton = null): static
+    {
+        if ($this->isCompiled) {
+            throw new ContainerException('Container is already compiled.');
+        }
+        parent::set($id, $definition, $arguments, $isSingleton);
+
+        return $this;
     }
 
     public function resource(iterable $resources): DiContainerCompilerInterface
@@ -28,6 +41,12 @@ class DiContainerCompiler extends DiContainer implements DiContainerCompilerInte
 
     public function compile(): DiContainerCallInterface&DiContainerInterface
     {
+        if (file_exists($this->compiledContainerFile)) {
+            $this->isCompiled = true;
+
+            return require_once $this->compiledContainerFile;
+        }
+
         throw new \LogicException('Compiling is not implemented');
     }
 }
