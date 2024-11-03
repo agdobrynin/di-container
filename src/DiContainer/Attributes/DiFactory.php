de9e41c4247ab100ee777a9656fa0c47b3c85db2
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-04 00:37:48 +0400

    refactor: [DiFactory attribute] Attribute can be repeatable on property.
    * DiFactory::makeFromReflection - return array with DiFactory items.

--- src/DiContainer/Attributes/DiFactory.php
+++ src/DiContainer/Attributes/DiFactory.php
@@ -7,7 +7,7 @@ namespace Kaspi\DiContainer\Attributes;
 use Kaspi\DiContainer\Exception\AutowiredAttributeException;
 use Kaspi\DiContainer\Interfaces\DiFactoryInterface;
 
-#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_PARAMETER)]
+#[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_PARAMETER | \Attribute::IS_REPEATABLE)]
 final class DiFactory
 {
     /**
@@ -19,12 +19,21 @@ final class DiFactory
             || throw new AutowiredAttributeException("Parameter '{$this->id}' must be implement '".DiFactoryInterface::class."' interface");
     }
 
-    public static function makeFromReflection(\ReflectionClass|\ReflectionParameter $parameter): ?self
+    /**
+     * @return array<int, DiFactory>
+     */
+    public static function makeFromReflection(\ReflectionClass|\ReflectionParameter $parameter): array
     {
         $attributes = $parameter->getAttributes(self::class);
 
-        return [] === $attributes
-            ? null
-            : $attributes[0]->newInstance();
+        if ($parameter instanceof \ReflectionClass && \count($attributes) > 1) {
+            throw new AutowiredAttributeException('The attribute #[DiFactory] can only be applied once per class.');
+        }
+
+        if ($parameter instanceof \ReflectionParameter && !$parameter->isVariadic() && \count($attributes) > 1) {
+            throw new AutowiredAttributeException('The attribute #[DiFactory] can only be applied once per non-variadic parameter.');
+        }
+
+        return \array_map(static fn (\ReflectionAttribute $attribute) => $attribute->newInstance(), $attributes);
     }
 }
