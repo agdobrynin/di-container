b4e07c74ed387f58c8e7497b23a2c623eb06e3cf
Alex Dobrynin <ag.dobrynin@gmail.com>
2024-11-11 11:54:54 +0400

    fix: [function] Check if function <diDefinition> exist. (#76)

--- src/DiContainer/function.php
+++ src/DiContainer/function.php
@@ -8,18 +8,20 @@ use Kaspi\DiContainer\Exception\ContainerException;
 use Kaspi\DiContainer\Interfaces\DiContainerInterface;
 use Kaspi\DiContainer\Interfaces\Exceptions\DiContainerConfigExceptionInterface;
 
-/**
- * @phan-suppress PhanUnreferencedFunction
- *
- * @throws DiContainerConfigExceptionInterface
- */
-function diDefinition(?string $containerKey = null, mixed $definition = null, ?array $arguments = null, ?bool $isSingleton = null): array
-{
-    $prepareDefinition = (
-        ($definition ? [0 => $definition] : [])
-        + ($arguments ? [DiContainerInterface::ARGUMENTS => $arguments] : [])
-        + (null !== $isSingleton ? [DiContainerInterface::SINGLETON => $isSingleton] : [])
-    ) ?: throw new ContainerException('Definition function is empty.');
+if (!\function_exists('Kaspi\DiContainer\diDefinition')) {
+    /**
+     * @phan-suppress PhanUnreferencedFunction
+     *
+     * @throws DiContainerConfigExceptionInterface
+     */
+    function diDefinition(?string $containerKey = null, mixed $definition = null, ?array $arguments = null, ?bool $isSingleton = null): array
+    {
+        $prepareDefinition = (
+            ($definition ? [0 => $definition] : [])
+            + ($arguments ? [DiContainerInterface::ARGUMENTS => $arguments] : [])
+            + (null !== $isSingleton ? [DiContainerInterface::SINGLETON => $isSingleton] : [])
+        ) ?: throw new ContainerException('Definition function is empty.');
 
-    return $containerKey ? [$containerKey => $prepareDefinition] : $prepareDefinition;
+        return $containerKey ? [$containerKey => $prepareDefinition] : $prepareDefinition;
+    }
 }
