559c8e83176e5aa25e061b35eb6b2fe774bfa953
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-21 16:59:05 +0400

    dev: [Test] Update tests.

---
+++ tests/Traits/AttributeReader/DiFactory/Fixtures/MainFirstDiFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Traits\AttributeReader\DiFactory\Fixtures;
+
+use Kaspi\DiContainer\Interfaces\DiFactoryInterface;
+use Psr\Container\ContainerInterface;
+
+class MainFirstDiFactory implements DiFactoryInterface
+{
+    public function __invoke(ContainerInterface $container): Main
+    {
+        return new Main();
+    }
+}
