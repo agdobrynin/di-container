c414dc697fc63424a4c98486ed3edde56b2760d8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-21 13:03:31 +0400

    dev: [Test] Update tests.

---
+++ tests/DiContainerConfig/MainTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DiContainerConfig;
+
+use Kaspi\DiContainer\DiContainerConfig;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ *
+ * @internal
+ */
+class MainTest extends TestCase
+{
+    public function testDefaultValues(): void
+    {
+        $config = new DiContainerConfig();
+
+        $this->assertTrue($config->isUseZeroConfigurationDefinition());
+        $this->assertTrue($config->isUseAttribute());
+        $this->assertFalse($config->isSingletonServiceDefault());
+    }
+
+    public function testUserValues(): void
+    {
+        $config = new DiContainerConfig(
+            useZeroConfigurationDefinition: false,
+            useAttribute: false,
+            isSingletonServiceDefault: true,
+        );
+
+        $this->assertFalse($config->isUseZeroConfigurationDefinition());
+        $this->assertFalse($config->isUseAttribute());
+        $this->assertTrue($config->isSingletonServiceDefault());
+    }
+}
