f6f2e1a608739f5068c120cbdd688b14ff5e8e63
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-22 11:30:53 +0400

    dev: [Test] Update tests.

---
+++ tests/Traits/PsrContainerTrait/PsrContainerTraitTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Traits\PsrContainerTrait;
+
+use Kaspi\DiContainer\Interfaces\Exceptions\ContainerNeedSetExceptionInterface;
+use Kaspi\DiContainer\Traits\PsrContainerTrait;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @covers \Kaspi\DiContainer\Traits\PsrContainerTrait
+ *
+ * @internal
+ */
+class PsrContainerTraitTest extends TestCase
+{
+    use PsrContainerTrait;
+
+    public function testPsrContainerSet(): void
+    {
+        $container = new class implements ContainerInterface {
+            public function get(string $id) {}
+
+            public function has(string $id): bool {}
+        };
+
+        $this->assertInstanceOf(self::class, $this->setContainer($container));
+        $this->assertInstanceOf(ContainerInterface::class, $this->getContainer());
+    }
+
+    public function testPsrContainerGetException(): void
+    {
+        $this->expectException(ContainerNeedSetExceptionInterface::class);
+        $this->expectExceptionMessage('Need set container implementation');
+
+        $this->getContainer();
+    }
+}
