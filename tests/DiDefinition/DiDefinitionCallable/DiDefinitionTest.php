545b6a5e10ffe09e753fe81dd54f8271ea259de6
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-22 22:48:26 +0400

    dev: [Tests] Update tests.

---
+++ tests/DiDefinition/DiDefinitionCallable/DiDefinitionTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DiDefinition\DiDefinitionCallable;
+
+use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiContainerConfig;
+use Kaspi\DiContainer\DiDefinition\DiDefinitionCallable;
+use Kaspi\DiContainer\Interfaces\Exceptions\ContainerNeedSetExceptionInterface;
+use PHPUnit\Framework\TestCase;
+use Tests\DiDefinition\DiDefinitionCallable\Fixtures\CallableArgument;
+use Tests\DiDefinition\DiDefinitionCallable\Fixtures\MainClass;
+
+use function Kaspi\DiContainer\diAutowire;
+
+/**
+ * @covers \Kaspi\DiContainer\diAutowire
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionCallable
+ *
+ * @internal
+ */
+class DiDefinitionTest extends TestCase
+{
+    public function testGetDefinitionWhenDefinitionIsCallable(): void
+    {
+        $this->assertEquals('log', (new DiDefinitionCallable('log'))->getDefinition());
+    }
+
+    public function testGetDefinitionAndParseDefinitionWithoutContainerFail(): void
+    {
+        $this->expectException(ContainerNeedSetExceptionInterface::class);
+
+        (new DiDefinitionCallable(MainClass::class.'::getServiceName'))->getDefinition();
+    }
+
+    public function testGetDefinitionAndParseDefinitionSuccess(): void
+    {
+        $container = new DiContainer([
+            diAutowire(MainClass::class)
+                ->addArgument('serviceName', 'someServiceName'),
+        ]);
+
+        $definition = (new DiDefinitionCallable([MainClass::class, 'getServiceName']))
+            ->setContainer($container)
+            ->getDefinition()
+        ;
+
+        $this->assertIsCallable($definition);
+        $this->assertInstanceOf(MainClass::class, $definition[0]);
+        $this->assertEquals('getServiceName', $definition[1]);
+    }
+
+    public function testCallableArgument(): void
+    {
+        $def = (new DiDefinitionCallable(CallableArgument::class))
+            ->addArgument('name', 'ok')
+        ;
+        $def->setContainer(new DiContainer(config: new DiContainerConfig()));
+
+        $this->assertEquals('ok 😀', $def->invoke());
+    }
+
+    public function testCallableArguments(): void
+    {
+        $def = (new DiDefinitionCallable(CallableArgument::class))
+            ->addArguments(['name' => 'ok'])
+        ;
+        $def->setContainer(new DiContainer(config: new DiContainerConfig()));
+
+        $this->assertEquals('ok 😀', $def->invoke());
+    }
+}
---
+++ tests/DiDefinition/DiDefinitionCallable/Fixtures/MainClass.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DiDefinition\DiDefinitionCallable\Fixtures;
+
+class MainClass
+{
+    public function __construct(private string $serviceName) {}
+
+    public function getServiceName(): string
+    {
+        return $this->serviceName;
+    }
+}
