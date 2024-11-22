8af5e3f26c2c31b9a5d28e78bebe67ede4ea65b0
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-22 13:13:23 +0400

    dev: [Test] Update tests.

---
+++ tests/DiContainerSetTest.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiDefinition\DiDefinitionValue;
+use Kaspi\DiContainer\Interfaces\Exceptions\ContainerAlreadyRegisteredExceptionInterface;
+use Kaspi\DiContainer\Interfaces\Exceptions\DiDefinitionExceptionInterface;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Kaspi\DiContainer\DiContainer
+ *
+ * @internal
+ */
+class DiContainerSetTest extends TestCase
+{
+    public function dataProviderWrongIdentifier(): \Generator
+    {
+        yield 'empty string' => [''];
+
+        yield 'spaces' => ['   '];
+    }
+
+    /**
+     * @dataProvider dataProviderWrongIdentifier
+     */
+    public function testWrongIdentifier(string $id): void
+    {
+        $this->expectException(DiDefinitionExceptionInterface::class);
+        $this->expectExceptionMessage('must be a non-empty string');
+
+        (new DiContainer())->set($id, 'foo');
+    }
+
+    public function dataProviderSuccessIdentifier(): \Generator
+    {
+        yield 'with spaces' => [' foo ', 'definition'];
+
+        yield 'with string as "null" and definition DiDefinition' => ['null', new DiDefinitionValue(null)];
+    }
+
+    /**
+     * @dataProvider dataProviderSuccessIdentifier
+     */
+    public function testSuccessIdentifier(string $id, mixed $definition): void
+    {
+        $container = (new DiContainer())->set($id, $definition);
+
+        $this->assertTrue($container->has($id));
+    }
+
+    public function testIdentifierNotUnique(): void
+    {
+        $container = (new DiContainer())->set('key', 'value');
+
+        $this->expectException(ContainerAlreadyRegisteredExceptionInterface::class);
+
+        $container->set('key', 'value2');
+    }
+}
