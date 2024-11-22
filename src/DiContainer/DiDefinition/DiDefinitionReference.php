0ef82d335c436c03329943e30d0af096bef25ee3
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-22 10:23:41 +0400

    feat: [DiDefinitionReference] Identifier must-be non-empty string

--- src/DiContainer/DiDefinition/DiDefinitionReference.php
+++ src/DiContainer/DiDefinition/DiDefinitionReference.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer\DiDefinition;
 
+use Kaspi\DiContainer\Exception\DiDefinitionException;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionInterface;
 
 final class DiDefinitionReference implements DiDefinitionInterface
 {
-    public function __construct(private string $containerIdentifier) {}
+    /**
+     * @param non-empty-string $containerIdentifier
+     */
+    public function __construct(private string $containerIdentifier)
+    {
+        if ('' === \trim($containerIdentifier)) {
+            throw new DiDefinitionException('Definition identifier must be a non-empty string.');
+        }
+    }
 
     public function getDefinition(): string
     {
