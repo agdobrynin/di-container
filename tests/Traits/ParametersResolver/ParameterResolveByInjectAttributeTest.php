06347599574da8ba9f374d82293113d31776dfa2
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-24 00:30:17 +0400

    dev: [Tests] Update tests.

--- tests/Traits/ParametersResolver/ParameterResolveByInjectAttributeTest.php
+++ tests/Traits/ParametersResolver/ParameterResolveByInjectAttributeTest.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace Tests\Traits\ParametersResolver;
 
 use Kaspi\DiContainer\Attributes\Inject;
+use Kaspi\DiContainer\Exception\AutowiredAttributeException;
 use Kaspi\DiContainer\Traits\ParametersResolverTrait;
 use Kaspi\DiContainer\Traits\PsrContainerTrait;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use Tests\Traits\ParametersResolver\Fixtures\SuperClass;
+use Tests\Traits\ParametersResolver\Fixtures\SuperInterface;
 
 /**
  * @covers \Kaspi\DiContainer\Attributes\Inject::getIdentifier
@@ -47,5 +50,34 @@ class ParameterResolveByInjectAttributeTest extends TestCase
             ['✔', '❤'],
             \call_user_func_array($fn, $arguments)->getArrayCopy()
         );
+    }
+
+    public function testParameterResolveTypedArgumentByInjectAttributeThrowManyInjectNonVariadic(): void
+    {
+        $fn = static fn (
+            #[Inject('a')]
+            #[Inject('b')]
+            SuperClass $iterator
+        ) => $iterator;
+        $this->reflectionParameters = (new \ReflectionFunction($fn))->getParameters();
+
+        $mockContainer = $this->createMock(ContainerInterface::class);
+        $mockContainer->expects($this->never())->method('get');
+        $this->setContainer($mockContainer);
+
+        $this->expectException(AutowiredAttributeException::class);
+        $this->expectExceptionMessage('once per non-variadic parameter');
+
+        $this->resolveParameters(useAttribute: true);
+    }
+
+    public function testParameterResolveTypedArgumentByInjectAttributeWithId(): void
+    {
+        $fn = static fn (
+            #[Inject('services.one')]
+            #[Inject('services.tow')]
+            SuperInterface ...$super
+        ) => $super;
+        $this->reflectionParameters = (new \ReflectionFunction($fn))->getParameters();
     }
 }
