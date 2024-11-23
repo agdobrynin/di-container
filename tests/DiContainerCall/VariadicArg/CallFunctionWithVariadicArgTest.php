d200a15db76366a51f86cfd543ca4cc2af33ae59
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-23 12:13:45 +0400

    dev: [Test] Update tests.

--- tests/DiContainerCall/VariadicArg/CallFunctionWithVariadicArgTest.php
+++ tests/DiContainerCall/VariadicArg/CallFunctionWithVariadicArgTest.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Tests\DiContainerCall\VariadicArg;
 
+use Kaspi\DiContainer\Attributes\Inject;
 use Kaspi\DiContainer\DiContainer;
 use Kaspi\DiContainer\DiContainerConfig;
 use PHPUnit\Framework\TestCase;
-use Tests\Fixtures\ClassWithSimplePublicProperty;
+use Tests\DiContainerCall\VariadicArg\Fixtures\WordHello;
+use Tests\DiContainerCall\VariadicArg\Fixtures\WordInterface;
+use Tests\DiContainerCall\VariadicArg\Fixtures\WordSuffix;
+use Tests\DiContainerCall\VariadicArg\Fixtures\WordVariadicDiFactory;
 
 use function Kaspi\DiContainer\diAutowire;
 use function Kaspi\DiContainer\diReference;
@@ -26,57 +30,99 @@ use function Kaspi\DiContainer\diReference;
  */
 class CallFunctionWithVariadicArgTest extends TestCase
 {
-    public function testUserFunctionVariadicArgumentsPassByCallMethod(): void
+    public function testUserFunctionVariadicArgumentsPassByCallMethodWithDiReference(): void
     {
-        $container = new DiContainer([
-            'item.first' => diAutowire(ClassWithSimplePublicProperty::class)
-                ->addArgument('publicProperty', 'Hello'),
-            'item.second' => diAutowire(ClassWithSimplePublicProperty::class)
-                ->addArgument('publicProperty', 'World'),
-        ]);
+        $fn = static fn (
+            WordInterface ...$word
+        ) => $word;
+
+        $definitions = [
+            'services.word_hello' => diAutowire(WordHello::class),
+            'services.word_suffix' => diAutowire(WordSuffix::class),
+        ];
+        $container = new DiContainer($definitions);
 
         $res = $container->call(
-            '\Tests\Fixtures\functionWithVariadic',
+            $fn,
             [
-                'item' => [ // <-- wrap variadic argument
-                    diReference('item.first'),
-                    diReference('item.second'),
-                ], // <-- wrap variadic argument
+                'word' => [
+                    diReference('services.word_hello'),
+                    diReference('services.word_suffix'),
+                ],
             ]
         );
 
-        $this->assertEquals(' / Hello / World', $res);
+        $this->assertInstanceOf(WordHello::class, $res[0]);
+        $this->assertInstanceOf(WordSuffix::class, $res[1]);
     }
 
-    public function testUserFunctionVariadicArgumentsByAttribute(): void
+    public function testUserFunctionVariadicArgumentsByAttributeManyTimes(): void
     {
+        $fn = static fn (
+            #[Inject('services.word_suffix')]
+            #[Inject('services.word_hello')]
+            WordInterface ...$word
+        ) => $word;
+
         $definitions = [
-            'item.first' => diAutowire(ClassWithSimplePublicProperty::class)
-                ->addArgument('publicProperty', '🎈'),
-            'item.second' => diAutowire(ClassWithSimplePublicProperty::class)
-                ->addArgument('publicProperty', '🎃'),
+            'services.word_hello' => diAutowire(WordHello::class),
+            'services.word_suffix' => diAutowire(WordSuffix::class),
         ];
-        $config = new DiContainerConfig(useAttribute: true);
-        $container = new DiContainer($definitions, $config);
+        $container = new DiContainer($definitions, new DiContainerConfig(useZeroConfigurationDefinition: false));
 
-        $res = $container->call('\Tests\Fixtures\functionWithVariadic');
+        $res = $container->call($fn);
 
-        $this->assertEquals(' / 🎈 / 🎃', $res);
+        $this->assertInstanceOf(WordSuffix::class, $res[0]);
+        $this->assertInstanceOf(WordHello::class, $res[1]);
     }
 
-
-    public function testUserFunctionVariadicArgumentsConfigToOneToMany(): void
+    public function testUserFunctionVariadicArgumentsInjectByContainerIdToOneToMany(): void
     {
+        $fn = static fn (
+            #[Inject('services.words')]
+            WordInterface ...$word
+        ) => $word;
 
+        $definitions = [
+            'services.words' => diAutowire(WordVariadicDiFactory::class),
+        ];
+        $container = new DiContainer($definitions, new DiContainerConfig());
+
+        $res = $container->call($fn);
+
+        $this->assertInstanceOf(WordSuffix::class, $res[0]);
+        $this->assertInstanceOf(WordHello::class, $res[1]);
     }
 
     public function testUserFunctionVariadicArgumentsByInjectWithIdAsReferenceToOneToMany(): void
     {
+        $fn = static fn (
+            WordInterface ...$word
+        ) => $word;
 
+        $definitions = [
+            'services.words' => diAutowire(WordVariadicDiFactory::class),
+        ];
+        $container = new DiContainer($definitions, new DiContainerConfig());
+
+        $res = $container->call($fn, ['word' => diReference('services.words')]);
+
+        $this->assertInstanceOf(WordSuffix::class, $res[0]);
+        $this->assertInstanceOf(WordHello::class, $res[1]);
     }
 
     public function testUserFunctionVariadicArgumentsByInjectWithIdAsDiFactoryOneToMany(): void
     {
+        $fn = static fn (
+            #[Inject(WordVariadicDiFactory::class)]
+            WordInterface ...$word
+        ) => $word;
 
+        $container = new DiContainer(config: new DiContainerConfig());
+
+        $res = $container->call($fn);
+
+        $this->assertInstanceOf(WordSuffix::class, $res[0]);
+        $this->assertInstanceOf(WordHello::class, $res[1]);
     }
 }
--- tests/Fixtures/function.php
+++ tests/Fixtures/function.php
@@ -14,16 +14,6 @@ function funcWithDependencyClass(
     return $class->publicProperty.($append ? ' + '.$append : '');
 }
 
-function functionWithVariadic(
-    #[Inject('item.first')]
-    #[Inject('item.second')]
-    ClassWithSimplePublicProperty ...$item
-): string {
-    return \array_reduce($item, static function (string $carry, ClassWithSimplePublicProperty $class): string {
-        return $carry.' / '.$class->publicProperty;
-    }, '');
-}
-
 function functionResolveArgumentByName(#[Inject] array $allUsers): string
 {
     return \implode(' - ', \array_map(static fn (string $item) => \strtoupper($item), $allUsers));
