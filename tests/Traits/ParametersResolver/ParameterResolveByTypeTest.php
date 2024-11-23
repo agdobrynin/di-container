00bdd32bcd607ca0364b6dc5f89e88059f891e14
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-23 15:30:21 +0400

    dev: [Tests] Update tests.

--- tests/Traits/ParametersResolver/ParameterResolveByTypeTest.php
+++ tests/Traits/ParametersResolver/ParameterResolveByTypeTest.php
@@ -21,7 +21,7 @@ class ParameterResolveByTypeTest extends TestCase
     use ParametersResolverTrait;
     use PsrContainerTrait;
 
-    public function testParameterResolveByTypeSuccess(): void
+    public function testParameterResolveByType(): void
     {
         $fn = static fn (\ArrayIterator $array) => $array;
         $this->reflectionParameters = (new \ReflectionFunction($fn))->getParameters();
@@ -29,6 +29,21 @@ class ParameterResolveByTypeTest extends TestCase
         $mock = $this->createMock(ContainerInterface::class);
         $mock->expects($this->once())
             ->method('get')->with(\ArrayIterator::class)
+            ->willReturn(new \ArrayIterator())
+        ;
+        $this->container = $mock;
+
+        $this->assertInstanceOf(\ArrayIterator::class, $this->resolveParameters()[0]);
+    }
+
+    public function testParameterResolveByName(): void
+    {
+        $fn = static fn ($myArrayIterator) => $myArrayIterator;
+        $this->reflectionParameters = (new \ReflectionFunction($fn))->getParameters();
+
+        $mock = $this->createMock(ContainerInterface::class);
+        $mock->expects($this->once())
+            ->method('get')->with('myArrayIterator')
             ->willReturn(new \ArrayIterator())
         ;
         $this->container = $mock;
