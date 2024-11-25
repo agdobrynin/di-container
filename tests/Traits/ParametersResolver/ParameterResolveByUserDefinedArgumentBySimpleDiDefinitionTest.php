3034374d7b8c1267a83a40356acd1680525c5405
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-25 15:22:04 +0400

    dev: [Test] Update tests.

---
+++ tests/Traits/ParametersResolver/ParameterResolveByUserDefinedArgumentBySimpleDiDefinitionTest.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Traits\ParametersResolver;
+
+use Kaspi\DiContainer\Traits\ParametersResolverTrait;
+use Kaspi\DiContainer\Traits\PsrContainerTrait;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+use function Kaspi\DiContainer\diValue;
+
+/**
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionValue
+ * @covers \Kaspi\DiContainer\diValue
+ * @covers \Kaspi\DiContainer\Traits\ParametersResolverTrait
+ * @covers \Kaspi\DiContainer\Traits\PsrContainerTrait
+ *
+ * @internal
+ */
+class ParameterResolveByUserDefinedArgumentBySimpleDiDefinitionTest extends TestCase
+{
+    // 🔥 Test Trait 🔥
+    use ParametersResolverTrait;
+    // 🧨 need for abstract method getContainer.
+    use PsrContainerTrait;
+
+    public function testUserDefinedArgumentByDiValueNonVariadicSuccess(): void
+    {
+        $fn = static fn (iterable $iterator) => $iterator;
+        $this->reflectionParameters = (new \ReflectionFunction($fn))->getParameters();
+
+        $mockContainer = $this->createMock(ContainerInterface::class);
+        $mockContainer->expects($this->never())->method('get');
+        $this->setContainer($mockContainer);
+
+        // 🚩 test data
+        $this->arguments = [
+            'iterator' => diValue(['aaa', 'bbb', 'ccc']),
+        ];
+
+        $res = \call_user_func_array($fn, $this->resolveParameters());
+
+        $this->assertEquals(['aaa', 'bbb', 'ccc'], $res);
+    }
+
+    public function testUserDefinedArgumentByManyDiValueVariadicSuccess(): void
+    {
+        $fn = static fn (iterable ...$iterator) => $iterator;
+        $this->reflectionParameters = (new \ReflectionFunction($fn))->getParameters();
+
+        $mockContainer = $this->createMock(ContainerInterface::class);
+        $mockContainer->expects($this->never())->method('get');
+        $this->setContainer($mockContainer);
+
+        // 🚩 test data
+        $this->arguments = [
+            'iterator' => [
+                diValue(['aaa', 'bbb', 'ccc']),
+                diValue(['ddd', 'eee', 'fff']),
+            ],
+        ];
+
+        $res = \call_user_func_array($fn, $this->resolveParameters());
+
+        $this->assertCount(2, $res);
+
+        $this->assertEquals(['aaa', 'bbb', 'ccc'], $res[0]);
+        $this->assertEquals(['ddd', 'eee', 'fff'], $res[1]);
+    }
+
+    public function testUserDefinedArgumentByOneDiValueVariadicSuccess(): void
+    {
+        $fn = static fn (iterable ...$iterator) => $iterator;
+        $this->reflectionParameters = (new \ReflectionFunction($fn))->getParameters();
+
+        $mockContainer = $this->createMock(ContainerInterface::class);
+        $mockContainer->expects($this->never())->method('get');
+        $this->setContainer($mockContainer);
+
+        // 🚩 test data
+        $this->arguments = [
+            'iterator' => diValue([['aaa', 'bbb', 'ccc'], ['ddd', 'eee', 'fff']]),
+        ];
+
+        $res = \call_user_func_array($fn, $this->resolveParameters());
+
+        $this->assertCount(2, $res);
+
+        $this->assertEquals(['aaa', 'bbb', 'ccc'], $res[0]);
+        $this->assertEquals(['ddd', 'eee', 'fff'], $res[1]);
+    }
+}
