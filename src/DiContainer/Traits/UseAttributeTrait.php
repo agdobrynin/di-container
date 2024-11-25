6396cab96894629a0e38476b1a86e953c5f9bbc8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-25 17:39:11 +0400

    Revert "fix: [UseAttributeTrait] Exception throw if property $useAttribute not set."
    This reverts commit 1ee6c3f303ab3d0e573510b85fb332f963e13e2b.

--- src/DiContainer/Traits/UseAttributeTrait.php
+++ src/DiContainer/Traits/UseAttributeTrait.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer\Traits;
 
-use Kaspi\DiContainer\Exception\DiDefinitionException;
-
 trait UseAttributeTrait
 {
     protected bool $useAttribute = false;
 
     public function isUseAttribute(): bool
     {
-        return $this->useAttribute ?? throw new DiDefinitionException('Need set $useAttribute. Use method setUseAttribute() in '.__CLASS__.' class.');
+        return $this->useAttribute;
     }
 
     /**
