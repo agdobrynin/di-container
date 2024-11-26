dd871dceaf8c237358e71f9a631fc16697a7f50f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-26 23:29:29 +0400

    dev: [Tests] Update tests.

---
+++ tests/DiContainer/ResolveSelfContainerTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DiContainer;
+
+use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiContainerConfig;
+use Kaspi\DiContainer\Interfaces\DiContainerInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ *
+ * @internal
+ */
+class ResolveSelfContainerTest extends TestCase
+{
+    public function dataProvider(): \Generator
+    {
+        yield 'ContainerInterface' => [ContainerInterface::class];
+
+        yield 'DiContainerInterface' => [DiContainerInterface::class];
+
+        yield 'DiContainer' => [DiContainer::class];
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testResolveWithoutConfig(string $id): void
+    {
+        $this->assertInstanceOf(DiContainer::class, (new DiContainer())->get($id));
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testResolveWithConfig(string $id): void
+    {
+        $this->assertInstanceOf(DiContainer::class, (new DiContainer(config: new DiContainerConfig()))->get($id));
+    }
+}
