178a5648f863c75ca3ab7adf2c319de1ccdb71ba
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-28 16:23:45 +0400

    dev: [Test] Update tests.

---
+++ tests/FromDocs/PhpDefinitions/DiCallableDefinitionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FromDocs\PhpDefinitions;
+
+use Kaspi\DiContainer\DiContainerFactory;
+use PHPUnit\Framework\TestCase;
+use Tests\FromDocs\PhpDefinitions\Fixtures\ServiceOne;
+
+use function Kaspi\DiContainer\diCallable;
+
+/**
+ * @covers \Kaspi\DiContainer\diCallable
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiContainerFactory
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionCallable
+ * @covers \Kaspi\DiContainer\Traits\UseAttributeTrait
+ *
+ * @internal
+ */
+class DiCallableDefinitionTest extends TestCase
+{
+    public function testDiCallableDefinition(): void
+    {
+        $definitions = [
+            'services.one' => diCallable(
+                definition: static fn () => new ServiceOne(apiKey: 'my-api-key'),
+                isSingleton: true,
+            ),
+        ];
+
+        $container = (new DiContainerFactory())->make($definitions);
+
+        $this->assertInstanceOf(ServiceOne::class, $container->get('services.one'));
+        $this->assertEquals('my-api-key', $container->get('services.one')->getApiKey());
+        $this->assertSame($container->get('services.one'), $container->get('services.one'));
+    }
+}
