7a83c3c2f389472be8d66d32493dc76cd38d2cf7
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-11-28 18:45:17 +0400

    dev: [Test] Update tests.

---
+++ tests/FromDocs/PhpDefinitions/Fixtures/ClassWithStaticMethods.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FromDocs\PhpDefinitions\Fixtures;
+
+class ClassWithStaticMethods
+{
+    public static function doSomething(
+        ServiceLocation $serviceLocation // Внедрение зависимости по типу
+    ): \stdClass {
+        return (object) [
+            'name' => 'John Doe',
+            'age' => 32,
+            'gender' => 'male',
+            'city' => $serviceLocation->locationCity,
+        ];
+    }
+}
---
+++ tests/FromDocs/PhpDefinitions/StaticMethodAsDefinitionTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FromDocs\PhpDefinitions;
+
+use Kaspi\DiContainer\DiContainerFactory;
+use PHPUnit\Framework\TestCase;
+use Tests\FromDocs\PhpDefinitions\Fixtures\ServiceLocation;
+
+use function Kaspi\DiContainer\diAutowire;
+use function Kaspi\DiContainer\diCallable;
+
+/**
+ * @covers \Kaspi\DiContainer\diAutowire
+ * @covers \Kaspi\DiContainer\diCallable
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiContainerFactory
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionCallable
+ * @covers \Kaspi\DiContainer\Traits\ParameterTypeByReflectionTrait
+ * @covers \Kaspi\DiContainer\Traits\UseAttributeTrait
+ *
+ * @internal
+ */
+class StaticMethodAsDefinitionTest extends TestCase
+{
+    public function testStaticMethodAsDefinition(): void
+    {
+        $defServices = [
+            diAutowire(ServiceLocation::class)
+                ->addArguments(['locationCity' => 'Vice city']),
+        ];
+
+        // ... many definitions ...
+
+        $defCustom = [
+            // Статический метод класса является callable типом.
+            // При вызове метода автоматически внедрится зависимость по типу ServiceLocation.
+            'doSomething' => diCallable('Tests\FromDocs\PhpDefinitions\Fixtures\ClassWithStaticMethods::doSomething'),
+        ];
+
+        $container = (new DiContainerFactory())->make(
+            \array_merge($defServices, $defCustom)
+        );
+
+        $res = $container->get('doSomething');
+        $expect = (object) ['name' => 'John Doe', 'age' => 32, 'gender' => 'male', 'city' => 'Vice city'];
+
+        $this->assertEquals($expect, $res);
+    }
+}
