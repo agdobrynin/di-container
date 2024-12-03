e89630d4006d5f952dab6ed95124b26f1b314697
Alex Dobrynin <ag.dobrynin@gmail.com>
2024-12-03 16:19:18 +0400

    Performance improve (#102)
    * refactor: [ParametersResolverTrait] Bind arguments without foreach.
    
    * refactor: [ParametersResolverTrait] Refactoring internal methods for get input argument.

--- src/DiContainer/Traits/ParametersResolverTrait.php
+++ src/DiContainer/Traits/ParametersResolverTrait.php
@@ -8,7 +8,6 @@ use Kaspi\DiContainer\DiDefinition\DiDefinitionGet;
 use Kaspi\DiContainer\Exception\AutowireAttributeException;
 use Kaspi\DiContainer\Exception\AutowireException;
 use Kaspi\DiContainer\Exception\CallCircularDependencyException;
-use Kaspi\DiContainer\Exception\InputArgumentNotFoundException;
 use Kaspi\DiContainer\Exception\NotFoundException;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionAutowireInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionInterface;
@@ -41,7 +40,7 @@ trait ParametersResolverTrait
     /**
      * User defined parameters by parameter name.
      *
-     * @var array<string, mixed>
+     * @var array<int|string, mixed>
      */
     protected array $arguments = [];
 
@@ -79,11 +78,7 @@ trait ParametersResolverTrait
      */
     public function bindArguments(mixed ...$argument): static
     {
-        $this->arguments = [];
-
-        foreach ($argument as $name => $value) {
-            $this->arguments[$name] = $value;
-        }
+        $this->arguments = $argument;
 
         return $this;
     }
@@ -104,8 +99,8 @@ trait ParametersResolverTrait
         self::$variadicPosition = 0;
 
         foreach ($this->reflectionParameters as $parameter) {
-            try {
-                $argumentDefinition = $this->getInputArgument($parameter);
+            if (false !== ($argumentNameOrIndex = $this->getArgumentNameOrIndexByParameter($parameter))) {
+                $argumentDefinition = $this->getInputArgument($argumentNameOrIndex, $parameter->isVariadic());
 
                 if (\is_array($argumentDefinition) && $parameter->isVariadic()) {
                     foreach ($argumentDefinition as $definitionItem) {
@@ -125,7 +120,6 @@ trait ParametersResolverTrait
                 \array_push($dependencies, ...$vals);
 
                 continue;
-            } catch (InputArgumentNotFoundException) {
             }
 
             $autowireException = null;
@@ -269,33 +263,34 @@ trait ParametersResolverTrait
         }
     }
 
-    protected function getInputArgument(\ReflectionParameter $parameter): mixed
+    protected function getArgumentNameOrIndexByParameter(\ReflectionParameter $parameter): false|int|string
     {
-        if (\array_key_exists($parameter->name, $this->arguments)) {
-            return $this->arguments[$parameter->name];
+        return match (true) {
+            [] === $this->arguments => false,
+            \array_key_exists($parameter->name, $this->arguments) => $parameter->name,
+            \array_key_exists($parameter->getPosition(), $this->arguments) => $parameter->getPosition(),
+            default => false,
+        };
+    }
+
+    protected function getInputArgument(int|string $argumentNameOrIndex, bool $isVariadic): mixed
+    {
+        if (\is_string($argumentNameOrIndex)) {
+            return $this->arguments[$argumentNameOrIndex];
         }
 
-        if (!$parameter->isVariadic() && \array_key_exists($parameter->getPosition(), $this->arguments)) {
-            return $this->arguments[$parameter->getPosition()];
+        if (!$isVariadic || !\array_key_exists($argumentNameOrIndex + 1, $this->arguments)) {
+            return $this->arguments[$argumentNameOrIndex];
         }
 
-        if ($parameter->isVariadic() && \array_key_exists($parameter->getPosition(), $this->arguments)) {
-            $variadicPosition = $parameter->getPosition();
+        $values = [];
+        $variadicPosition = $argumentNameOrIndex;
 
-            if (!\array_key_exists($variadicPosition + 1, $this->arguments)) {
-                return $this->arguments[$variadicPosition];
-            }
+        do {
+            $values[] = $this->arguments[$variadicPosition];
+            ++$variadicPosition;
+        } while (\array_key_exists($variadicPosition, $this->arguments));
 
-            $values = [];
-
-            do {
-                $values[] = $this->arguments[$variadicPosition];
-                ++$variadicPosition;
-            } while (\array_key_exists($variadicPosition, $this->arguments));
-
-            return $values;
-        }
-
-        throw new InputArgumentNotFoundException();
+        return $values;
     }
 }
