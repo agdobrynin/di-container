9a8f08c52b154618385841c18ba2122b83755e4f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-12-09 18:18:04 +0400

    fix: [Helper function] Helper function return interfaces.

--- src/DiContainer/function.php
+++ src/DiContainer/function.php
@@ -10,13 +10,16 @@ use Kaspi\DiContainer\DiDefinition\DiDefinitionGet;
 use Kaspi\DiContainer\DiDefinition\DiDefinitionProxyClosure;
 use Kaspi\DiContainer\DiDefinition\DiDefinitionReference;
 use Kaspi\DiContainer\DiDefinition\DiDefinitionValue;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionAutowireInterface;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionInterface;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionProxyClosureInterface;
 
 // @todo Remove alias when remove function diReference.
 \class_alias(DiDefinitionGet::class, 'Kaspi\DiContainer\DiDefinition\DiDefinitionReference'); // @codeCoverageIgnore
 
 if (!\function_exists('Kaspi\DiContainer\diAutowire')) { // @codeCoverageIgnore
     // @phan-suppress-next-line PhanUnreferencedFunction
-    function diAutowire(string $definition, ?bool $isSingleton = null): DiDefinitionAutowire
+    function diAutowire(string $definition, ?bool $isSingleton = null): DiDefinitionAutowireInterface
     {
         return new DiDefinitionAutowire($definition, $isSingleton);
     }
@@ -24,7 +27,7 @@ if (!\function_exists('Kaspi\DiContainer\diAutowire')) { // @codeCoverageIgnore
 
 if (!\function_exists('Kaspi\DiContainer\diCallable')) { // @codeCoverageIgnore
     // @phan-suppress-next-line PhanUnreferencedFunction
-    function diCallable(array|callable|string $definition, ?bool $isSingleton = null): DiDefinitionCallable
+    function diCallable(array|callable|string $definition, ?bool $isSingleton = null): DiDefinitionAutowireInterface
     {
         return new DiDefinitionCallable($definition, $isSingleton);
     }
@@ -50,7 +53,7 @@ if (!\function_exists('Kaspi\DiContainer\diGet')) { // @codeCoverageIgnore
     /**
      * @phan-suppress PhanUnreferencedFunction
      */
-    function diGet(string $containerIdentifier): DiDefinitionGet
+    function diGet(string $containerIdentifier): DiDefinitionInterface
     {
         return new DiDefinitionGet($containerIdentifier);
     }
@@ -60,7 +63,7 @@ if (!\function_exists('Kaspi\DiContainer\diValue')) { // @codeCoverageIgnore
     /**
      * @phan-suppress PhanUnreferencedFunction
      */
-    function diValue(mixed $value): DiDefinitionValue
+    function diValue(mixed $value): DiDefinitionInterface
     {
         return new DiDefinitionValue($value);
     }
@@ -70,7 +73,7 @@ if (!\function_exists('Kaspi\DiContainer\diProxyClosure')) { // @codeCoverageIgn
     /**
      * @phan-suppress PhanUnreferencedFunction
      */
-    function diProxyClosure(string $definition): DiDefinitionProxyClosure
+    function diProxyClosure(string $definition): DiDefinitionProxyClosureInterface
     {
         return new DiDefinitionProxyClosure($definition);
     }
