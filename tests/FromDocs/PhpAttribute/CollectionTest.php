6fbf05c23573a358fbebc358f8e70e348535a684
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-12-11 21:08:15 +0400

    dev: [Tests] Update tests.

---
+++ tests/FromDocs/PhpAttribute/CollectionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FromDocs\PhpAttribute;
+
+use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiContainerConfig;
+use PHPUnit\Framework\TestCase;
+use Tests\FromDocs\PhpAttribute\Fixtures\IterableArg;
+use Tests\FromDocs\PhpAttribute\Fixtures\RuleA;
+use Tests\FromDocs\PhpAttribute\Fixtures\RuleB;
+use Tests\FromDocs\PhpAttribute\Fixtures\RuleInterface;
+
+use function Kaspi\DiContainer\diCallable;
+
+/**
+ * @covers \Kaspi\DiContainer\Attributes\Inject
+ * @covers \Kaspi\DiContainer\diCallable
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionCallable
+ * @covers \Kaspi\DiContainer\Traits\ParameterTypeByReflectionTrait
+ * @covers \Kaspi\DiContainer\Traits\UseAttributeTrait
+ *
+ * @internal
+ */
+class CollectionTest extends TestCase
+{
+    public function testCollection(): void
+    {
+        $definitions = [
+            'services.rule-list' => diCallable(
+                definition: static fn (RuleA $a, RuleB $b) => \func_get_args(),
+                isSingleton: true
+            ),
+        ];
+
+        $container = new DiContainer(
+            $definitions,
+            new DiContainerConfig(
+                useAttribute: true // use attributes
+            )
+        );
+
+        $class = $container->get(IterableArg::class);
+
+        foreach ($class->getValues() as $item) {
+            $this->assertInstanceOf(RuleInterface::class, $item);
+            $this->assertContains($item::class, [RuleA::class, RuleB::class]);
+        }
+    }
+}
