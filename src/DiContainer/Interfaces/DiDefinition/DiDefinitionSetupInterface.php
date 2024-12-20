c243f0094d0f2144d716971e8939d23ebc30a584
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-12-20 19:15:40 +0400

    feat: [DiDefinitionSetupInterface] Update docblock.

--- src/DiContainer/Interfaces/DiDefinition/DiDefinitionSetupInterface.php
+++ src/DiContainer/Interfaces/DiDefinition/DiDefinitionSetupInterface.php
@@ -9,6 +9,8 @@ interface DiDefinitionSetupInterface extends DiDefinitionArgumentsInterface
     /**
      * Call setter method for class with input arguments.
      * Calling method may use autowire feature.
+     *
+     * @param non-empty-string $method
      */
     public function setup(string $method, mixed ...$argument): static;
 }
