bf89ce0d6e79e81fa5e621704e83473c2b94e475
Alexey Dobrynin <ag.dobrynin@gmail.com>
2024-12-20 21:12:09 +0400

    feat: [DiDefinitionSetupInterface] Update docblock.

--- src/DiContainer/Interfaces/DiDefinition/DiDefinitionSetupInterface.php
+++ src/DiContainer/Interfaces/DiDefinition/DiDefinitionSetupInterface.php
@@ -9,6 +9,18 @@ interface DiDefinitionSetupInterface extends DiDefinitionArgumentsInterface
     /**
      * Call setter method for class with input arguments.
      * Calling method may use autowire feature.
+     * This method can be used many times.
+     * Arguments provided by the user added by name or index.
+     *
+     * User can set arguments by named argument:
+     *
+     *       setup('classMethod', var1: 'value 1', var2: 'value 2')
+     *       // bind parameters by name Class->classMethod(var1: 'value 1', var2: 'value 2')
+     *
+     * User can set arguments by index argument:
+     *
+     *      setup('classMethod', 'value 1', 'value 2')
+     *      // bind parameters by index Class->classMethod('value 1', 'value 2')
      *
      * @param non-empty-string $method
      */
