3623284836f2a80d247e03a7abae578888d09d65
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-01-24 13:41:54 +0400

    feat: [DiDefinitionProxyClosure] Use TagsTrait.

--- src/DiContainer/DiDefinition/DiDefinitionProxyClosure.php
+++ src/DiContainer/DiDefinition/DiDefinitionProxyClosure.php
@@ -6,13 +6,16 @@ namespace Kaspi\DiContainer\DiDefinition;
 
 use Kaspi\DiContainer\Exception\AutowireException;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionInvokableInterface;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiTaggedDefinitionInterface;
 use Kaspi\DiContainer\Traits\PsrContainerTrait;
+use Kaspi\DiContainer\Traits\TagsTrait;
 use Kaspi\DiContainer\Traits\UseAttributeTrait;
 
-final class DiDefinitionProxyClosure implements DiDefinitionInvokableInterface
+final class DiDefinitionProxyClosure implements DiDefinitionInvokableInterface, DiTaggedDefinitionInterface
 {
     use PsrContainerTrait;
     use UseAttributeTrait;
+    use TagsTrait;
 
     /**
      * @param non-empty-string $definition
