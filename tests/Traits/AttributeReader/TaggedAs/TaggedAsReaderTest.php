e7efee5892da5df6ba53e7a2fff1ab2e45880e93
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-01-28 17:18:48 +0400

    dev: [Tests] Update tests.

---
+++ tests/Traits/AttributeReader/TaggedAs/TaggedAsReaderTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Traits\AttributeReader\TaggedAs;
+
+use Kaspi\DiContainer\Attributes\TaggedAs;
+use Kaspi\DiContainer\Interfaces\Exceptions\AutowireExceptionInterface;
+use Kaspi\DiContainer\Traits\AttributeReaderTrait;
+use Kaspi\DiContainer\Traits\DiContainerTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Kaspi\DiContainer\Attributes\TaggedAs
+ * @covers \Kaspi\DiContainer\Traits\AttributeReaderTrait
+ *
+ * @internal
+ */
+class TaggedAsReaderTest extends TestCase
+{
+    protected $reader;
+
+    public function setUp(): void
+    {
+        $this->reader = new class {
+            use AttributeReaderTrait {
+                getTaggedAsAttribute as public;
+            }
+            use DiContainerTrait; // abstract method cover.
+        };
+    }
+
+    public function tearDown(): void
+    {
+        $this->reader = null;
+    }
+
+    public function testNonTaggedAs(): void
+    {
+        $fn = static fn (iterable $tagged) => '';
+        $p = new \ReflectionParameter($fn, 0);
+
+        $this->assertFalse($this->reader->getTaggedAsAttribute($p)->valid());
+    }
+
+    public function testTaggedAsManyForNonVariadic(): void
+    {
+        $fn = static fn (
+            #[TaggedAs('tags.handlers-opa')]
+            #[TaggedAs('tags.voters-security')]
+            iterable $tagged
+        ) => '';
+        $p = new \ReflectionParameter($fn, 0);
+
+        $this->expectException(AutowireExceptionInterface::class);
+        $this->expectExceptionMessage('can only be applied once per non-variadic parameter');
+
+        /** @var \Generator<TaggedAs> $res */
+        $res = $this->reader->getTaggedAsAttribute($p);
+
+        $this->assertFalse($res->valid());
+    }
+
+    public function testTaggedAsManyForVariadic(): void
+    {
+        $fn = static fn (
+            #[TaggedAs('tags.handlers-opa')]
+            #[TaggedAs('tags.voters-security', false)]
+            ...$tagged
+        ) => [];
+        $p = new \ReflectionParameter($fn, 0);
+
+        /** @var \Generator<TaggedAs> $res */
+        $res = $this->reader->getTaggedAsAttribute($p);
+
+        $this->assertTrue($res->valid());
+        $this->assertEquals('tags.handlers-opa', $res->current()->getIdentifier());
+        $this->assertTrue($res->current()->isLazy());
+
+        $res->next();
+
+        $this->assertEquals('tags.voters-security', $res->current()->getIdentifier());
+        $this->assertFalse($res->current()->isLazy());
+    }
+}
