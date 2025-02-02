773f8c03076bca54f998f147aea6710891f0a131
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-02-02 11:47:38 +0400

    dev: [Tests] Update tests.

---
+++ tests/DiDefinition/DiDefinitionTaggedAs/TaggedAsThroughContainerVariadicTest.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DiDefinition\DiDefinitionTaggedAs;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+class TaggedAsThroughContainerVariadicTest extends TestCase {}
