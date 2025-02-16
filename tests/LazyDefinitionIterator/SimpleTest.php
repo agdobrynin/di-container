0fc315440b348677453a515675399722fa678819
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-02-16 21:41:05 +0400

    feat: [LazyDefinitionIterator] Implement interface \ArrayAccess.

--- tests/LazyDefinitionIterator/SimpleTest.php
+++ tests/LazyDefinitionIterator/SimpleTest.php
@@ -15,14 +15,43 @@ use Psr\Container\ContainerInterface;
  */
 class SimpleTest extends TestCase
 {
+    private object $container;
+
+    public function setUp(): void
+    {
+        $this->container = $this->createMock(ContainerInterface::class);
+    }
+
     public function testIsIterableAndEmpty(): void
     {
-        $mockContainer = $this->createMock(ContainerInterface::class);
-        $li = new LazyDefinitionIterator($mockContainer, []);
+        $li = new LazyDefinitionIterator($this->container, []);
 
         $this->assertIsIterable($li);
         $this->assertFalse($li->valid());
         $this->assertNull($li->current());
         $this->assertNull($li->key());
+    }
+
+    public function testArrayAccessMagicIsset(): void
+    {
+        $li = new LazyDefinitionIterator($this->container, ['ok' => 'something']);
+
+        $this->assertTrue(isset($li['ok']));
+        $this->assertFalse(isset($li['any']));
+    }
+
+    public function testByOffsetExists(): void
+    {
+        $li = new LazyDefinitionIterator($this->container, ['ok' => 'something']);
+
+        $this->assertTrue($li->offsetExists('ok'));
+        $this->assertFalse($li->offsetExists('any'));
+    }
+
+    public function testByHas(): void
+    {
+        $li = new LazyDefinitionIterator($this->container, ['ok' => 'something']);
+
+        $this->assertTrue($li->has('ok'));
     }
 }
