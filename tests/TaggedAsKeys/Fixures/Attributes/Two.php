30cfe6ea18767761330d1b789294e6d5ad329714
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-02-18 14:42:46 +0400

    dev: [Tests] Update tests.

--- tests/TaggedAsKeys/Fixures/Attributes/Two.php
+++ tests/TaggedAsKeys/Fixures/Attributes/Two.php
@@ -7,4 +7,11 @@ namespace Tests\TaggedAsKeys\Fixures\Attributes;
 use Kaspi\DiContainer\Attributes\Tag;
 
 #[Tag('tags.one', options: ['key.override' => 'key-service'], priority: 0)]
-final class Two {}
+#[Tag('tags.some-service', options: ['key' => 'some_service.two', 'key.method' => 'self::getKey'], priority: 10)]
+final class Two
+{
+    public static function getKey(): string
+    {
+        return 'some_service.Dos';
+    }
+}
