0cc6909e001dde515cc6e7a4320dc3a41ecbd01d
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-03-04 21:26:29 +0400

    WIP

--- src/DiContainer/Interfaces/Finder/FinderClassInterface.php
+++ src/DiContainer/Interfaces/Finder/FinderClassInterface.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer\Interfaces\Finder;
 
+/**
+ * Find classes and interfaces in source files.
+ */
 interface FinderClassInterface
 {
     /**
+     * Get classes and interfaces.
+     *
      * @return \Iterator<non-negative-int, class-string>
      *
      * @throws \RuntimeException
