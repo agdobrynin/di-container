4c383eb6e5a13c4cad48ea5d7c425dc5069cccac
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-03-04 16:42:54 +0400

    WIP - code review fix.

--- tests/Traits/AttributeReader/AutowireExclude/AutowireExcludeTest.php
+++ tests/Traits/AttributeReader/AutowireExclude/AutowireExcludeTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\Traits\AttributeReader\AutowireExclude;
 
 use Kaspi\DiContainer\Traits\AttributeReaderTrait;
-use Kaspi\DiContainer\Traits\DiContainerTrait;
 use PHPUnit\Framework\TestCase;
 use Tests\Traits\AttributeReader\AutowireExclude\Fixtures\ClassWillBeExcluded;
 use Tests\Traits\AttributeReader\AutowireExclude\Fixtures\ClassWillNotBeExcluded;
@@ -18,7 +17,6 @@ use Tests\Traits\AttributeReader\AutowireExclude\Fixtures\ClassWillNotBeExcluded
 class AutowireExcludeTest extends TestCase
 {
     use AttributeReaderTrait;
-    use DiContainerTrait;
 
     public function testAutowireExclude(): void
     {
