19b256d74c5687c37acb6cea3ca3939078c83ee8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-03-05 15:39:07 +0400

    dev: [Tests] Update tests.

--- tests/DiContainer/Has/Fixtures/ExcludeClass.php
+++ tests/DiContainer/Has/Fixtures/ExcludeClass.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\DiContainer\Has\Fixtures;
 
+use Kaspi\DiContainer\Attributes\Autowire;
 use Kaspi\DiContainer\Attributes\AutowireExclude;
 
+#[Autowire] // always ignore because use attribute AutowireExclude
 #[AutowireExclude]
 final class ExcludeClass {}
---
+++ tests/DiContainer/ResolveByAutowireAttribute/ResolveByAutowireTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\DiContainer\ResolveByAutowireAttribute;
+
+use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiContainerConfig;
+use PHPUnit\Framework\TestCase;
+use Tests\DiContainer\ResolveByAutowireAttribute\Fixtures\One;
+use Tests\DiContainer\ResolveByAutowireAttribute\Fixtures\Two;
+
+/**
+ * @covers \Kaspi\DiContainer\Attributes\Autowire
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire
+ *
+ * @internal
+ */
+class ResolveByAutowireTest extends TestCase
+{
+    public function testAutowireAttributeWithSingletonTrueButContainerSetDefaultSingletonFalse(): void
+    {
+        $container = new DiContainer(config: new DiContainerConfig(
+            isSingletonServiceDefault: false,
+        ));
+
+        $one = $container->get(One::class);
+
+        $this->assertSame($one, $container->get(One::class));
+    }
+
+    public function testAutowireAttributeWithSingletonFalseButContainerSetDefaultSingletonTrue(): void
+    {
+        $container = new DiContainer(config: new DiContainerConfig(
+            isSingletonServiceDefault: true,
+        ));
+
+        $two = $container->get(Two::class);
+
+        $this->assertNotSame($two, $container->get(Two::class));
+    }
+}
