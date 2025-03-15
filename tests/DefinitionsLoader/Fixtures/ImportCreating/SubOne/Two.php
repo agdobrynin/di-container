1b4c3edf127f466879f4f5bac54b558df3adbc61
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-03-15 22:41:18 +0400

    dev: [Tests] Update tests.

--- tests/DefinitionsLoader/Fixtures/ImportCreating/SubOne/Two.php
+++ tests/DefinitionsLoader/Fixtures/ImportCreating/SubOne/Two.php
@@ -4,4 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\DefinitionsLoader\Fixtures\ImportCreating\SubOne;
 
+use Kaspi\DiContainer\Attributes\Autowire;
+
+#[Autowire(isSingleton: true)]
 final class Two {}
