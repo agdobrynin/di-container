423fb6012b0e981571ffac97a880ae9a2d029038
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-03-21 13:34:59 +0400

    WIP

--- tests/FinderClosureCode/FInderClosureCodeTest.php
+++ tests/FinderClosureCode/FInderClosureCodeTest.php
@@ -4,20 +4,26 @@ declare(strict_types=1);
 namespace Tests\FinderClosureCode;
 
 use Kaspi\DiContainer\Finder\FinderClosureCode;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionAutowireInterface;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use SplFileObject;
 
 class FInderClosureCodeTest extends TestCase {
     public function testFunction(): void
     {
         $code = (new FinderClosureCode())
-            ->getCode(static fn(ContainerInterface $c): \stdClass => $c->get('services.exclude'));
+            ->getCode(static fn(ContainerInterface $c): FinderClosureCode|DiDefinitionAutowireInterface =>
+                $c->has('services.exclude')
+                    ? $c->get('services.exclude')
+                    : new FinderClosureCode()
+            );
 
         var_export($code);
 
         $code = (new FinderClosureCode())
-            ->getCode(static function(): FinderClosureCode {
-                $someArg = 'aaa';
+            ->getCode(static function(string $filename): FinderClosureCode {
+                $someArg = new SplFileObject($filename);
 
                 return new FinderClosureCode($someArg);
             });
