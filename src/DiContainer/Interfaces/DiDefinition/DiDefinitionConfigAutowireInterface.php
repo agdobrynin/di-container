7a95056456a8d8d574e09366f77900a27abf3a76
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-10-10 18:35:43 +0400

    feat: [DiDefinitionConfigAutowireInterface] Add method for setup immutable service.

--- src/DiContainer/Interfaces/DiDefinition/DiDefinitionConfigAutowireInterface.php
+++ src/DiContainer/Interfaces/DiDefinition/DiDefinitionConfigAutowireInterface.php
@@ -7,7 +7,7 @@ namespace Kaspi\DiContainer\Interfaces\DiDefinition;
 interface DiDefinitionConfigAutowireInterface extends DiDefinitionArgumentsInterface
 {
     /**
-     * Call setter method for class with input arguments.
+     * Call setter method for class with input arguments without return type aka void.
      * Calling method may use autowire feature.
      * This method can be used many times.
      * Arguments provided by the user added by name or index.
@@ -28,4 +28,26 @@ interface DiDefinitionConfigAutowireInterface extends DiDefinitionArgumentsInter
      * @return $this
      */
     public function setup(string $method, mixed ...$argument): static;
+
+    /**
+     * Call immutable setter method for class with input arguments with return type aka self.
+     * Calling method may use autowire feature.
+     * This method can be used many times.
+     * Arguments provided by the user added by name or index.
+     *
+     * User can set arguments by named argument:
+     *
+     *       setupImmutable('classMethod', var1: 'value 1', var2: 'value 2')
+     *       // bind parameters by name Class->classMethod(var1: 'value 1', var2: 'value 2')
+     *
+     * User can set arguments by index argument:
+     *
+     *      setupImmutable('classMethod', 'value 1', 'value 2')
+     *      // bind parameters by index Class->classMethod('value 1', 'value 2')
+     *
+     * @param non-empty-string $method
+     *
+     * @return $this
+     */
+    public function setupImmutable(string $method, mixed ...$argument): static;
 }
