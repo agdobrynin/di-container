a0bab68bfd4cf8f7fb4b862d095d09b18857265e
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-10-23 19:27:00 +0400

    dev: [code style] New rules for php-cs-fixer

--- src/DiContainer/DiDefinition/DiDefinitionAutowire.php
+++ src/DiContainer/DiDefinition/DiDefinitionAutowire.php
@@ -206,7 +206,7 @@ final class DiDefinitionAutowire implements DiDefinitionConfigAutowireInterface,
      * @param non-empty-string $name
      * @param TagOptions       $operationOptions
      */
-    public function geTagPriority(string $name, array $operationOptions = []): null|int|string
+    public function geTagPriority(string $name, array $operationOptions = []): int|string|null
     {
         if (null !== ($priority = $this->internalGeTagPriority($name, $operationOptions))) {
             return $priority;
