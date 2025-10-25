ff572b31205039739220110e5a1233ccde2cef3e
Alex Dobrynin <ag.dobrynin@gmail.com>
2025-10-25 21:04:25 +0400

    docs: [02-attribute-definition] Update documentation page for feature use PHP attribute `#[Autowire]`. (#229)
    dev: [Tests] Update test for feature use `#[Autowire]`.
    (cherry picked from commit 29bc6692b9d90ede6b5a559ddef327f4b31a436b)

---
+++ tests/Attributes/MultiAutowireTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Attributes;
+
+use Kaspi\DiContainer\DefinitionsLoader;
+use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiContainerConfig;
+use PHPUnit\Framework\TestCase;
+use Tests\Attributes\Fixtures\MultiAutowire;
+
+/**
+ * @covers \Kaspi\DiContainer\Attributes\Autowire
+ * @covers \Kaspi\DiContainer\DefinitionsLoader
+ * @covers \Kaspi\DiContainer\DiContainer
+ * @covers \Kaspi\DiContainer\DiContainerConfig
+ * @covers \Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire
+ * @covers \Kaspi\DiContainer\Finder\FinderFile
+ * @covers \Kaspi\DiContainer\Finder\FinderFullyQualifiedName
+ * @covers \Kaspi\DiContainer\ImportLoader
+ * @covers \Kaspi\DiContainer\ImportLoaderCollection
+ *
+ * @internal
+ */
+class MultiAutowireTest extends TestCase
+{
+    public function testMultiAutowireWithoutDefinitionsLoader(): void
+    {
+        $container = new DiContainer(
+            definitions: [],
+            config: new DiContainerConfig(
+                useZeroConfigurationDefinition: true,
+                useAttribute: true,
+                isSingletonServiceDefault: false,
+            )
+        );
+
+        $m = $container->get(MultiAutowire::class);
+
+        self::assertSame($m, $container->get(MultiAutowire::class));
+        // definition key not defined.
+        self::assertFalse($container->has('service.singleton'));
+    }
+
+    public function testMultiAutowireAndDefinitionsLoader(): void
+    {
+        $loader = (new DefinitionsLoader())
+            ->import(
+                namespace: 'Tests\Attributes\Fixtures\\',
+                src: __DIR__.'/Fixtures',
+                useAttribute: true,
+            )
+        ;
+
+        $container = new DiContainer(
+            definitions: $loader->definitions(),
+            config: new DiContainerConfig(
+                useZeroConfigurationDefinition: true,
+                useAttribute: true,
+                isSingletonServiceDefault: false,
+            )
+        );
+
+        $m = $container->get(MultiAutowire::class);
+        // definition key load from DefinitionsLoader::import()
+        $m1 = $container->get('service.singleton');
+
+        self::assertSame($m, $container->get(MultiAutowire::class));
+        self::assertSame($m1, $container->get('service.singleton'));
+    }
+}
