72ada10693908eac850f00209316f4f8ce99c8c6
Alex Dobrynin <ag.dobrynin@gmail.com>
2025-11-13 21:37:06 +0400

    dev: [Tests] Add tests for code coverage. (#283)

---
+++ tests/Traits/AttributeReader/AttributeOnParameter/AttributeOnParameterTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Traits\AttributeReader\AttributeOnParameter;
+
+use Generator;
+use Kaspi\DiContainer\Attributes\Inject;
+use Kaspi\DiContainer\Attributes\InjectByCallable;
+use Kaspi\DiContainer\Attributes\ProxyClosure;
+use Kaspi\DiContainer\Attributes\TaggedAs;
+use Kaspi\DiContainer\Exception\AutowireAttributeException;
+use Kaspi\DiContainer\Interfaces\DiContainerInterface;
+use Kaspi\DiContainer\Traits\AttributeReaderTrait;
+use PHPUnit\Framework\TestCase;
+use ReflectionFunction;
+use ReflectionParameter;
+
+/**
+ * @covers \Kaspi\DiContainer\Traits\AttributeReaderTrait
+ *
+ * @internal
+ */
+class AttributeOnParameterTest extends TestCase
+{
+    use AttributeReaderTrait;
+
+    /**
+     * @dataProvider dataProviderParam
+     */
+    public function testAttributeOnParameterIntersect(ReflectionParameter $param): void
+    {
+        $this->expectException(AutowireAttributeException::class);
+
+        $this->getAttributeOnParameter(
+            $param,
+            $this->createMock(DiContainerInterface::class)
+        )->valid();
+    }
+
+    public function dataProviderParam(): Generator
+    {
+        yield 'Inject and ProxyClosure' => [
+            (new ReflectionFunction(static fn (#[Inject, ProxyClosure('service.one')] $param) => true))->getParameters()[0],
+        ];
+
+        yield 'ProxyClosure and TaggedAs' => [
+            (new ReflectionFunction(static fn (#[ProxyClosure('service.one'), TaggedAs('tags.one')] $param) => true))->getParameters()[0],
+        ];
+
+        yield 'InjectByCallable and TaggedAs' => [
+            (new ReflectionFunction(static fn (#[TaggedAs('tags.one'), InjectByCallable('func2')] $param) => true))->getParameters()[0],
+        ];
+    }
+}
