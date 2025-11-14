9c01eddab3a943ffcee12cc707b239045e5e8a53
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-11-14 22:04:12 +0400

    feat: [AttributeReaderTrait] `DiFactoryInterface::__invoke()` must return the same class to which the `#[DiFactory]` attribute is applied.

---
+++ tests/Traits/AttributeReader/DiFactory/Fixtures/FactoryMainFailTwo.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Traits\AttributeReader\DiFactory\Fixtures;
+
+use Kaspi\DiContainer\Interfaces\DiFactoryInterface;
+use Psr\Container\ContainerInterface;
+
+final class FactoryMainFailTwo implements DiFactoryInterface
+{
+    public function __invoke(ContainerInterface $container): object
+    {
+        return new MainFailTwo();
+    }
+}
