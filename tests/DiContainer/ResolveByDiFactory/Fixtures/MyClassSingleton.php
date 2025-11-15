8ac81b8d4766184d5daf3cc5aff3610ac284a0bd
Alex Dobrynin <ag.dobrynin@gmail.com>
2025-11-15 11:19:11 +0400

    feat: [AttributeReaderTrait] `DiFactoryInterface::__invoke()` must return the same class to which the `#[DiFactory]` attribute is applied. (#286)

--- tests/DiContainer/ResolveByDiFactory/Fixtures/MyClassSingleton.php
+++ tests/DiContainer/ResolveByDiFactory/Fixtures/MyClassSingleton.php
@@ -6,7 +6,7 @@ namespace Tests\DiContainer\ResolveByDiFactory\Fixtures;
 
 use Kaspi\DiContainer\Attributes\DiFactory;
 
-#[DiFactory(MyClassDiFactory::class, isSingleton: true)]
+#[DiFactory(MyClassSingletonDiFactory::class, isSingleton: true)]
 class MyClassSingleton
 {
     public function __construct(public DependencyClass $dependency) {}
