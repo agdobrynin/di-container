48df7e2360cda44d9981f640be224237893cd05f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-11-30 17:20:46 +0400

    dev: code review fixes.

--- src/DiContainer/DiDefinition/Arguments/ArgumentResolver.php
+++ src/DiContainer/DiDefinition/Arguments/ArgumentResolver.php
@@ -8,6 +8,7 @@ use Kaspi\DiContainer\Exception\DiDefinitionException;
 use Kaspi\DiContainer\Helper;
 use Kaspi\DiContainer\Interfaces\DiContainerInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\Arguments\ArgumentBuilderInterface;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionArgumentsInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionInterface;
 use Kaspi\DiContainer\Interfaces\Exceptions\ArgumentBuilderExceptionInterface;
 use Kaspi\DiContainer\Interfaces\Exceptions\DiDefinitionExceptionInterface;
@@ -17,6 +18,9 @@ use function array_key_exists;
 use function is_int;
 use function sprintf;
 
+/**
+ * @phpstan-import-type BindArgumentsType from DiDefinitionArgumentsInterface
+ */
 final class ArgumentResolver
 {
     /**
@@ -26,13 +30,7 @@ final class ArgumentResolver
      */
     public static function resolve(ArgumentBuilderInterface $argBuilder, DiContainerInterface $container, ?DiDefinitionInterface $context = null): array
     {
-        $resolvedArgs = [];
-
-        foreach ($argBuilder->build() as $argNameOrIndex => $arg) {
-            $resolvedArgs[$argNameOrIndex] = self::resolveArg($arg, $argNameOrIndex, $argBuilder, $container, $context);
-        }
-
-        return $resolvedArgs;
+        return self::resolveArgs($argBuilder->build(), $argBuilder, $container, $context);
     }
 
     /**
@@ -42,46 +40,50 @@ final class ArgumentResolver
      */
     public static function resolveByPriorityBindArguments(ArgumentBuilderInterface $argBuilder, DiContainerInterface $container, ?DiDefinitionInterface $context = null): array
     {
-        $resolvedArgs = [];
-
-        foreach ($argBuilder->buildByPriorityBindArguments() as $argNameOrIndex => $arg) {
-            $resolvedArgs[$argNameOrIndex] = self::resolveArg($arg, $argNameOrIndex, $argBuilder, $container, $context);
-        }
-
-        return $resolvedArgs;
+        return self::resolveArgs($argBuilder->buildByPriorityBindArguments(), $argBuilder, $container, $context);
     }
 
     /**
+     * @param BindArgumentsType $args
+     *
+     * @return mixed[]
+     *
      * @throws DiDefinitionExceptionInterface
      */
-    private static function resolveArg(mixed $arg, int|string $argNameOrIndex, ArgumentBuilderInterface $argBuilder, DiContainerInterface $container, ?DiDefinitionInterface $context): mixed
+    private static function resolveArgs(array $args, ArgumentBuilderInterface $argBuilder, DiContainerInterface $container, ?DiDefinitionInterface $context): array
     {
-        try {
-            return $arg instanceof DiDefinitionInterface
-                ? $arg->resolve($container, $context)
-                : $arg;
-        } catch (ContainerExceptionInterface $e) {
-            if (is_int($argNameOrIndex)) {
-                $param = $argBuilder->getFunctionOrMethod()->getParameters()[$argNameOrIndex] ?? null;
-                $argPresentedBy = null !== $param && array_key_exists($param->getName(), $argBuilder->getBindArguments())
-                    ? $param->getName()
-                    : $argNameOrIndex;
-            } else {
-                $argPresentedBy = $argNameOrIndex;
-            }
+        $resolvedArgs = [];
 
-            $argMessage = is_int($argPresentedBy)
-                ? sprintf('at position #%d', $argPresentedBy)
-                : sprintf('by named argument $%s', $argPresentedBy);
+        foreach ($args as $argNameOrIndex => $arg) {
+            try {
+                $resolvedArgs[$argNameOrIndex] = $arg instanceof DiDefinitionInterface
+                    ? $arg->resolve($container, $context)
+                    : $arg;
+            } catch (ContainerExceptionInterface $e) {
+                if (is_int($argNameOrIndex)) {
+                    $param = $argBuilder->getFunctionOrMethod()->getParameters()[$argNameOrIndex] ?? null;
+                    $argPresentedBy = null !== $param && array_key_exists($param->getName(), $argBuilder->getBindArguments())
+                        ? $param->getName()
+                        : $argNameOrIndex;
+                } else {
+                    $argPresentedBy = $argNameOrIndex;
+                }
 
-            throw (
+                $argMessage = is_int($argPresentedBy)
+                    ? sprintf('at position #%d', $argPresentedBy)
+                    : sprintf('by named argument $%s', $argPresentedBy);
+
+                throw (
                 new DiDefinitionException(
                     message: sprintf('Cannot resolve parameter %s in %s.', $argMessage, Helper::functionName($argBuilder->getFunctionOrMethod())),
                     previous: $e
                 )
-            )
-                ->setContext(context_argument: $arg, context_fn_reflection: $argBuilder->getFunctionOrMethod())
-            ;
+                )
+                    ->setContext(context_argument: $arg, context_fn_reflection: $argBuilder->getFunctionOrMethod())
+                ;
+            }
         }
+
+        return $resolvedArgs;
     }
 }
