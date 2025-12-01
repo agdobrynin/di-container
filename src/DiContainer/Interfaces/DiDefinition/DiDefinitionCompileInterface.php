c9f98016e438655f597244b7869ea650ab7c0c5c
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-12-01 22:49:52 +0400

    feat: [DiDefinitionCompileInterface] Interfaces for compiling definition.

---
+++ src/DiContainer/Interfaces/DiDefinition/DiDefinitionCompileInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\DiContainer\Interfaces\DiDefinition;
+
+use Kaspi\DiContainer\Interfaces\Exceptions\DiDefinitionCompileExceptionInterface;
+
+interface DiDefinitionCompileInterface
+{
+    /**
+     * @throws DiDefinitionCompileExceptionInterface
+     */
+    public function compile(): string;
+}
