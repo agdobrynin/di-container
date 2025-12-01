a35605af20b4a2276ee8e1584e66ef451a6b24d7
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-12-01 22:50:09 +0400

    feat: [DiDefinitionValue] Compile definition to string.

--- src/DiContainer/DiDefinition/DiDefinitionValue.php
+++ src/DiContainer/DiDefinition/DiDefinitionValue.php
@@ -4,13 +4,24 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer\DiDefinition;
 
+use InvalidArgumentException;
+use Kaspi\DiContainer\Exception\DiDefinitionCompileException;
 use Kaspi\DiContainer\Interfaces\DiContainerInterface;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionCompileInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionTagArgumentInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiTaggedDefinitionInterface;
 use Kaspi\DiContainer\Traits\TagsTrait;
+use UnitEnum;
 
-final class DiDefinitionValue implements DiDefinitionInterface, DiDefinitionTagArgumentInterface, DiTaggedDefinitionInterface
+use function array_walk_recursive;
+use function get_debug_type;
+use function is_array;
+use function is_scalar;
+use function sprintf;
+use function var_export;
+
+final class DiDefinitionValue implements DiDefinitionInterface, DiDefinitionTagArgumentInterface, DiTaggedDefinitionInterface, DiDefinitionCompileInterface
 {
     use TagsTrait;
 
@@ -27,5 +38,36 @@ final class DiDefinitionValue implements DiDefinitionInterface, DiDefinitionTagA
     public function resolve(DiContainerInterface $container, mixed $context = null): mixed
     {
         return $this->definition;
+    }
+
+    public function compile(): string
+    {
+        if (null === $this->definition || is_scalar($this->definition) || $this->definition instanceof UnitEnum) {
+            return var_export($this->definition, true);
+        }
+
+        if (is_array($this->definition)) {
+            try {
+                $tmp = $this->definition;
+
+                array_walk_recursive($tmp, function (mixed $item) {
+                    if (null !== $item && !is_scalar($item) && !($item instanceof UnitEnum)) {
+                        throw new InvalidArgumentException(sprintf('The value in array is invalid type "%s".', get_debug_type($item)));
+                    }
+                });
+
+                return var_export($this->definition, true);
+            } catch (InvalidArgumentException $exceptionInArray) {
+            }
+        }
+
+        $causedBy = isset($exceptionInArray)
+            ? ' Caused by: '.$exceptionInArray->getMessage()
+            : '';
+
+        throw new DiDefinitionCompileException(
+            sprintf('Cannot compile definition type "%s". Support only a scalar-type, null value, UnitEnum type or array with that types.'.$causedBy, get_debug_type($this->definition)),
+            previous: $exceptionInArray ?? null,
+        );
     }
 }
