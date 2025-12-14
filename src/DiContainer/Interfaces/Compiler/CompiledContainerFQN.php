75a1c42c7c58234800596b5bbffe85e8275795d8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-12-14 19:44:08 +0400

    dev: [CompiledContainerFQN] Update PhpDoc blocks.

--- src/DiContainer/Interfaces/Compiler/CompiledContainerFQN.php
+++ src/DiContainer/Interfaces/Compiler/CompiledContainerFQN.php
@@ -6,9 +6,28 @@ namespace Kaspi\DiContainer\Interfaces\Compiler;
 
 interface CompiledContainerFQN
 {
+    /**
+     * Namespace for compiled container class.
+     * If namespace not defined return empty string.
+     * Example:
+     *
+     *      class-string `\App\Services\CompiledContainer`
+     *      namespace `App\Services`
+     */
     public function getNamespace(): string;
 
+    /**
+     * Class name compiled container.
+     *
+     * @return class-string
+     */
     public function getClass(): string;
 
+    /**
+     * Fully qualified name of compiled container with lead `\`.
+     * Example:
+     *
+     *     Fully qualified class name '\App\Services\CompiledContainer'
+     */
     public function getFQN(): string;
 }
