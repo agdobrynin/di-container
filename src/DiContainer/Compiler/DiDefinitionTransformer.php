cad3d569cc75078d7769a4fa51807ded0a2120f3
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-12-14 19:49:02 +0400

    feat: [DiDefinitionTransformer] Rename class `DiDefinitionTransform` to `DiDefinitionTransformer` and implement method `DiDefinitionTransformer::transform()` for scalar definitions.

---
+++ src/DiContainer/Compiler/DiDefinitionTransformer.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\DiContainer\Compiler;
+
+use Kaspi\DiContainer\Compiler\DefinitionCompiler\Value;
+use Kaspi\DiContainer\DiDefinition\DiDefinitionValue;
+use Kaspi\DiContainer\Exception\DefinitionCompileException;
+use Kaspi\DiContainer\Interfaces\Compiler\CompilableDefinitionInterface;
+use Kaspi\DiContainer\Interfaces\Compiler\DiDefinitionTransformerInterface;
+
+use function gettype;
+use function sprintf;
+
+final class DiDefinitionTransformer implements DiDefinitionTransformerInterface
+{
+    public function transform(mixed $definition): CompilableDefinitionInterface
+    {
+        if ($definition instanceof DiDefinitionValue) {
+            return new Value($definition->getDefinition());
+        }
+
+        throw new DefinitionCompileException(sprintf('Unsupported definition type "%s"', gettype($definition)));
+    }
+}
