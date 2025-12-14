e505622560c96fe1f4a7411e9f0059c7b61b5008
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-12-14 14:26:10 +0400

    feat: [CompiledEntityInterface] Implement interface to class `CompiledEntity`.

---
+++ src/DiContainer/Compiler/CompiledEntry.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\DiContainer\Compiler;
+
+use Kaspi\DiContainer\Interfaces\Compiler\CompiledEntryInterface;
+
+final class CompiledEntry implements CompiledEntryInterface
+{
+    /**
+     * @param list<non-empty-string> $scopeVariables
+     * @param non-empty-string       $returnType
+     */
+    public function __construct(
+        private readonly string $expression,
+        private readonly ?bool $isSingleton,
+        private readonly string $statements = '',
+        private readonly string $scopeServiceVariableName = '',
+        private readonly array $scopeVariables = [],
+        private readonly string $returnType = 'mixed',
+    ) {}
+
+    public function getExpression(): string
+    {
+        return $this->expression;
+    }
+
+    public function getStatements(): string
+    {
+        return $this->statements;
+    }
+
+    public function getScopeServiceVariableName(): string
+    {
+        return $this->scopeServiceVariableName;
+    }
+
+    public function getScopeVariables(): array
+    {
+        return $this->scopeVariables;
+    }
+
+    public function isSingleton(): ?bool
+    {
+        return $this->isSingleton;
+    }
+
+    public function getReturnType(): string
+    {
+        return $this->returnType;
+    }
+}
---
+++ src/DiContainer/Interfaces/Compiler/CompiledEntryInterface.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\DiContainer\Interfaces\Compiler;
+
+interface CompiledEntryInterface
+{
+    /**
+     * TODO make description for this method.
+     */
+    public function getExpression(): string;
+
+    /**
+     * TODO make description for this method.
+     */
+    public function getStatements(): string;
+
+    /**
+     * TODO make description for this method.
+     */
+    public function getScopeServiceVariableName(): string;
+
+    /**
+     * TODO make description for this method.
+     *
+     * @return list<non-empty-string>
+     */
+    public function getScopeVariables(): array;
+
+    /**
+     * Compiled container entity is singleton.
+     * When value is `null` container entity resolve directly.
+     */
+    public function isSingleton(): ?bool;
+
+    /**
+     * Return type of compiled container entity.
+     *
+     * @return non-empty-string
+     */
+    public function getReturnType(): string;
+}
