e589adeac6cbf0ea65c8e7b6402ed0fe81ae9c88
Alexey Dobrynin <ag.dobrynin@gmail.com>
2025-12-23 18:57:28 +0400

    fix: [GetEntry] Some container IDs that need to be excluded from the definition iterator.

--- src/DiContainer/Compiler/CompilableDefinition/GetEntry.php
+++ src/DiContainer/Compiler/CompilableDefinition/GetEntry.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Kaspi\DiContainer\Compiler\CompilableDefinition;
 
 use Kaspi\DiContainer\Compiler\CompiledEntry;
+use Kaspi\DiContainer\DiContainer;
 use Kaspi\DiContainer\Exception\DefinitionCompileException;
 use Kaspi\DiContainer\Interfaces\Compiler\CompilableDefinitionInterface;
 use Kaspi\DiContainer\Interfaces\Compiler\CompiledEntryInterface;
 use Kaspi\DiContainer\Interfaces\Compiler\DiContainerDefinitionsInterface;
+use Kaspi\DiContainer\Interfaces\DiContainerInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionLinkInterface;
 use Kaspi\DiContainer\Interfaces\Exceptions\DiDefinitionExceptionInterface;
+use Psr\Container\ContainerInterface;
 
+use function in_array;
 use function sprintf;
 use function var_export;
 
@@ -30,7 +34,9 @@ final class GetEntry implements CompilableDefinitionInterface
             throw new DefinitionCompileException('Cannot compile reference definition.', previous: $e);
         }
 
-        $this->diContainerDefinitions->pushToDefinitionIterator($containerIdentifier);
+        if (!in_array($containerIdentifier, [ContainerInterface::class, DiContainerInterface::class, DiContainer::class], true)) {
+            $this->diContainerDefinitions->pushToDefinitionIterator($containerIdentifier);
+        }
 
         $expression = sprintf('%s->get(%s)', $containerVariableName, var_export($containerIdentifier, true));
 
