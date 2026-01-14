e253fce32d59d0d5d547759b43a3ca7a7b58cee5
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-01-14 11:04:31 +0400

    feat: [SourceDefinitionsMutableInterface] Update return `\Kaspi\DiContainer\Interfaces\SourceDefinitionsMutableInterface::getIterator()`.

--- src/DiContainer/Interfaces/SourceDefinitionsMutableInterface.php
+++ src/DiContainer/Interfaces/SourceDefinitionsMutableInterface.php
@@ -6,12 +6,13 @@ namespace Kaspi\DiContainer\Interfaces;
 
 use ArrayAccess;
 use IteratorAggregate;
+use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionIdentifierInterface;
 use Traversable;
 
 interface SourceDefinitionsMutableInterface extends ArrayAccess, IteratorAggregate
 {
     /**
-     * @return Traversable<non-empty-string, mixed>
+     * @return Traversable<non-empty-string, DiDefinitionIdentifierInterface|mixed>
      */
     public function getIterator(): Traversable;
 }
