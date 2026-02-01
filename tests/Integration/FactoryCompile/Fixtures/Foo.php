7abc40fb9855566eaf6f54dd811c71014a8e0e7e
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-02-01 18:25:50 +0400

    dev: [Tests] Add tests with arguments for `DiFactory` and `DiDefinitionFactory`.

--- tests/Integration/FactoryCompile/Fixtures/Foo.php
+++ tests/Integration/FactoryCompile/Fixtures/Foo.php
@@ -6,7 +6,10 @@ namespace Tests\Integration\FactoryCompile\Fixtures;
 
 use Kaspi\DiContainer\Attributes\DiFactory;
 
-#[DiFactory([FooFactoryStatic::class, 'create'])]
+#[DiFactory(
+    [FooFactoryStatic::class, 'create'],
+    arguments: ['newStr' => 'Lorem ipsum dolor sit amet']
+)]
 final class Foo
 {
     public function __construct(public string $strFoo) {}
