d28099cd7e2ee3294bef4e7dee1e5ab00dd4d56d
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-02-14 21:58:38 +0400

    feat: [DefinitionsConfiguratorInterface] Definitions configurator.

---
+++ src/DiContainer/Interfaces/DefinitionsConfiguratorInterface.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\DiContainer\Interfaces;
+
+use Kaspi\DiContainer\Interfaces\Exceptions\DefinitionsLoaderExceptionInterface;
+
+interface DefinitionsConfiguratorInterface
+{
+    /**
+     * Remove any definitions from configuration files or from importing classes.
+     *
+     * @param non-empty-string $id
+     */
+    public function removeDefinition(string $id): void;
+
+    /**
+     * Overwrites any definitions from configuration files or from importing classes.
+     *
+     * @param non-empty-string $id
+     */
+    public function setDefinition(string $id, mixed $definition): void;
+
+    /**
+     * Find a definition via container identifier.
+     *
+     * @param non-empty-string $id
+     *
+     * @throws DefinitionsLoaderExceptionInterface
+     */
+    public function getDefinition(string $id): mixed;
+
+    /**
+     * Load definitions from configuration files.
+     *
+     * @param non-empty-string $file
+     * @param non-empty-string ...$_
+     */
+    public function load(string $file, string ...$_): void;
+
+    /**
+     * Load definitions from configuration files with override previous definition.
+     *
+     * @param non-empty-string $file
+     * @param non-empty-string ...$_
+     */
+    public function loadOverride(string $file, string ...$_): void;
+}
