92dfcdf8633d3d8075447684d127fb13fc70c9de
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-02-14 11:33:49 +0400

    dev: [Tests] Add test for removed definition IDs on compiled container.

--- tests/DiContainer/RemovedDefinitionIds/RemovedDefinitionIdsTest.php
+++ tests/DiContainer/RemovedDefinitionIds/RemovedDefinitionIdsTest.php
@@ -4,17 +4,36 @@ declare(strict_types=1);
 
 namespace Tests\DiContainer\RemovedDefinitionIds;
 
+use Kaspi\DiContainer\AttributeReader;
+use Kaspi\DiContainer\Compiler\CompilableDefinition\ObjectEntry;
+use Kaspi\DiContainer\Compiler\CompiledEntries;
+use Kaspi\DiContainer\Compiler\CompiledEntry;
+use Kaspi\DiContainer\Compiler\ContainerCompiler;
+use Kaspi\DiContainer\Compiler\ContainerCompilerToFile;
+use Kaspi\DiContainer\Compiler\DiContainerDefinitions;
+use Kaspi\DiContainer\Compiler\DiDefinitionTransformer;
+use Kaspi\DiContainer\Compiler\IdsIterator;
+use Kaspi\DiContainer\DefinitionsLoader;
 use Kaspi\DiContainer\DiContainer;
+use Kaspi\DiContainer\DiContainerBuilder;
 use Kaspi\DiContainer\DiContainerConfig;
+use Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire;
 use Kaspi\DiContainer\Exception\NotFoundException;
+use Kaspi\DiContainer\Finder\FinderFile;
+use Kaspi\DiContainer\Finder\FinderFullyQualifiedName;
+use Kaspi\DiContainer\FinderFullyQualifiedNameCollection;
 use Kaspi\DiContainer\SourceDefinitions\AbstractSourceDefinitionsMutable;
 use Kaspi\DiContainer\SourceDefinitions\DeferredSourceDefinitionsMutable;
 use Kaspi\DiContainer\SourceDefinitions\ImmediateSourceDefinitionsMutable;
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\NotFoundExceptionInterface;
 use Tests\DiContainer\RemovedDefinitionIds\Fixtures\Bar;
 use Tests\DiContainer\RemovedDefinitionIds\Fixtures\Foo;
+
+use function bin2hex;
+use function random_bytes;
 
 /**
  * @internal
@@ -25,6 +44,21 @@ use Tests\DiContainer\RemovedDefinitionIds\Fixtures\Foo;
 #[CoversClass(AbstractSourceDefinitionsMutable::class)]
 #[CoversClass(ImmediateSourceDefinitionsMutable::class)]
 #[CoversClass(DeferredSourceDefinitionsMutable::class)]
+#[CoversClass(AttributeReader::class)]
+#[CoversClass(ObjectEntry::class)]
+#[CoversClass(CompiledEntry::class)]
+#[CoversClass(CompiledEntries::class)]
+#[CoversClass(ContainerCompiler::class)]
+#[CoversClass(ContainerCompilerToFile::class)]
+#[CoversClass(DiContainerDefinitions::class)]
+#[CoversClass(DiDefinitionTransformer::class)]
+#[CoversClass(IdsIterator::class)]
+#[CoversClass(DefinitionsLoader::class)]
+#[CoversClass(DiContainerBuilder::class)]
+#[CoversClass(DiDefinitionAutowire::class)]
+#[CoversClass(FinderFullyQualifiedNameCollection::class)]
+#[CoversClass(FinderFile::class)]
+#[CoversClass(FinderFullyQualifiedName::class)]
 class RemovedDefinitionIdsTest extends TestCase
 {
     public function testRemovedDefinitionIds(): void
@@ -75,6 +109,36 @@ class RemovedDefinitionIdsTest extends TestCase
             ),
             removedDefinitionIds: [Foo::class => true]
         );
+
+        $container->get(Foo::class);
+    }
+
+    public function testResolveRemovedDefinitionOnCompiledContainer(): void
+    {
+        $this->expectException(NotFoundExceptionInterface::class);
+
+        vfsStream::setup();
+
+        $container = (new DiContainerBuilder(
+            new DiContainerConfig(
+                useZeroConfigurationDefinition: true,
+                useAttribute: false,
+            )
+        ))
+            ->import(
+                'Tests\DiContainer\RemovedDefinitionIds\Fixtures\\',
+                __DIR__.'/Fixtures',
+                excludeFiles: [
+                    '*/Foo.php',
+                ]
+            )
+            ->compileToFile(
+                vfsStream::url('root/'),
+                'App\Container'.bin2hex(random_bytes(5)),
+                isExclusiveLockFile: false,
+            )
+            ->build()
+        ;
 
         $container->get(Foo::class);
     }
