eaab779f1af40ca76243eb7ecad2aa6e8176ac4f
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-02-18 15:30:11 +0400

    dev: [Tests] Add test for fining tagged definitions in definitions configurator.

--- tests/DefinitionsLoader/DefinitionsLoaderWithConfiguratorTest.php
+++ tests/DefinitionsLoader/DefinitionsLoaderWithConfiguratorTest.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Tests\DefinitionsLoader;
 
+use Kaspi\DiContainer\AttributeReader;
+use Kaspi\DiContainer\Attributes\Tag;
 use Kaspi\DiContainer\DefinitionsLoader;
 use Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire;
+use Kaspi\DiContainer\DiDefinition\DiDefinitionTaggedAs;
 use Kaspi\DiContainer\Finder\FinderFile;
 use Kaspi\DiContainer\Finder\FinderFullyQualifiedName;
 use Kaspi\DiContainer\FinderFullyQualifiedNameCollection;
@@ -17,6 +20,13 @@ use PHPUnit\Framework\Attributes\CoversFunction;
 use PHPUnit\Framework\TestCase;
 use Tests\DefinitionsLoader\Fixtures\DefinitionsConfigurator\Bar;
 use Tests\DefinitionsLoader\Fixtures\DefinitionsConfigurator\Foo;
+use Tests\DefinitionsLoader\Fixtures\TaggedAttr\Bat;
+use Tests\DefinitionsLoader\Fixtures\TaggedInterface\Baz;
+use Tests\DefinitionsLoader\Fixtures\TaggedInterface\QuxInterface;
+
+use function array_keys;
+use function Kaspi\DiContainer\diAutowire;
+use function Kaspi\DiContainer\diTaggedAs;
 
 /**
  * @internal
@@ -28,6 +38,10 @@ use Tests\DefinitionsLoader\Fixtures\DefinitionsConfigurator\Foo;
 #[CoversClass(Helper::class)]
 #[CoversClass(DiDefinitionAutowire::class)]
 #[CoversFunction('\Kaspi\DiContainer\diAutowire')]
+#[CoversFunction('\Kaspi\DiContainer\diTaggedAs')]
+#[CoversClass(DiDefinitionTaggedAs::class)]
+#[CoversClass(AttributeReader::class)]
+#[CoversClass(Tag::class)]
 class DefinitionsLoaderWithConfiguratorTest extends TestCase
 {
     public function testCircularLoadFromFile(): void
@@ -164,7 +178,7 @@ use Kaspi\DiContainer\Interfaces\DefinitionsConfiguratorInterface;
 use Tests\DefinitionsLoader\Fixtures\DefinitionsConfigurator\Foo;
 use Kaspi\DiContainer\DiDefinition\DiDefinitionAutowire;
         
-return static function (DefinitionsConfiguratorInterface $configurator) {
+return static function (DefinitionsConfiguratorInterface $configurator): void {
     $foo = $configurator->getDefinition(Foo::class);
     if ($foo instanceof DiDefinitionAutowire) {
         $foo->bindTag("tags.foo");    
@@ -189,5 +203,78 @@ return static function (DefinitionsConfiguratorInterface $configurator) {
                 self::assertTrue(isset($def->getBoundTags()['tags.foo']));
             }
         }
+    }
+
+    public function testConfiguratorFindTaggedDefinitionAsInterface(): void
+    {
+        vfsStream::setup(structure: [
+            'config1.php' => '<?php
+use Kaspi\DiContainer\Interfaces\DefinitionsConfiguratorInterface;
+use Tests\DefinitionsLoader\Fixtures\TaggedInterface\QuxInterface;
+        
+return static function (DefinitionsConfiguratorInterface $configurator): void {
+    foreach ($configurator->findTaggedDefinition(QuxInterface::class) as $def) {
+        $def->bindTag("tags.as_interface");    
+    }
+};',
+        ]);
+
+        $defs = (new DefinitionsLoader())
+            ->useAttribute(false)
+            ->import('Tests\DefinitionsLoader\Fixtures\TaggedInterface\\', __DIR__.'/Fixtures/TaggedInterface/')
+            ->addDefinitions(true, [
+                diAutowire(Baz::class)
+                    ->bindTag(QuxInterface::class),
+                'services.tagged' => diTaggedAs('tags.bat'),
+            ])
+            ->load(vfsStream::url('root/config1.php'))
+            ->definitions()
+        ;
+
+        $res = [...$defs];
+
+        self::assertFalse(isset($res[Baz::class]->getBoundTags()['tags.as_interface']));
+        self::assertTrue(isset($res[Fixtures\TaggedInterface\Bar::class]->getBoundTags()['tags.as_interface']));
+        self::assertTrue(isset($res[Fixtures\TaggedInterface\Foo::class]->getBoundTags()['tags.as_interface']));
+    }
+
+    public function testConfiguratorFindTaggedDefinition(): void
+    {
+        vfsStream::setup(structure: [
+            'config1.php' => '<?php
+use Kaspi\DiContainer\Interfaces\DefinitionsConfiguratorInterface;
+        
+return static function (DefinitionsConfiguratorInterface $configurator): void {
+    $collection = [];
+
+    foreach ($configurator->findTaggedDefinition("tags.one") as $id => $def) {
+        $collection[$id] = $def;    
+    }
+    
+    $configurator->setDefinition("collection.tags.one", $collection);
+};',
+        ]);
+
+        $defs = (new DefinitionsLoader())
+            ->useAttribute(true)
+            ->import('Tests\DefinitionsLoader\Fixtures\TaggedAttr\\', __DIR__.'/Fixtures/TaggedAttr/')
+            ->addDefinitions(true, [
+                diAutowire(Bat::class)
+                    ->bindTag('tags.one'),
+            ])
+            ->load(vfsStream::url('root/config1.php'))
+            ->definitions()
+        ;
+
+        $res = array_keys([...$defs]['collection.tags.one']);
+
+        self::assertEqualsCanonicalizing(
+            [
+                Fixtures\TaggedAttr\Bar::class,
+                Bat::class,
+                Fixtures\TaggedAttr\Baz::class,
+            ],
+            $res
+        );
     }
 }
