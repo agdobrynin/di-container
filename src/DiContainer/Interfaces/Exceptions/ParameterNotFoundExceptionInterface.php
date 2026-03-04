cda30a9b08109b111e8527b0754b07fd3abcbefe
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-03-04 12:26:31 +0400

    feat: [SourceParametersMutableInterface] Interface for container parameters.

---
+++ src/DiContainer/Interfaces/Exceptions/ParameterNotFoundExceptionInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaspi\DiContainer\Interfaces\Exceptions;
+
+use Psr\Container\NotFoundExceptionInterface;
+
+interface ParameterNotFoundExceptionInterface extends NotFoundExceptionInterface {}
