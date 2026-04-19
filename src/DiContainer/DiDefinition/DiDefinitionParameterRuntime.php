ec1065a5e3d60369aba16dd7c7b7ceb3d62e5791
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-04-19 16:37:29 +0400

    refactor: [DiDefinitionParameter, DiDefinitionParameterRuntime] Container parameter definitions extends `DiDefinitionParameterWithContextAbstract`.
    - `DiDefinitionParameterWithContextInterface` as common interface for container parameter with context.
    - `DiDefinitionParameterInterface` extends `DiDefinitionParameterWithContextInterface`.
    - `DiDefinitionParameterRuntimeInterface` extends `DiDefinitionParameterWithContextInterface`.

--- src/DiContainer/DiDefinition/DiDefinitionParameterRuntime.php
+++ src/DiContainer/DiDefinition/DiDefinitionParameterRuntime.php
@@ -10,26 +10,18 @@ use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionNoArgumentsInterface;
 use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionParameterRuntimeInterface;
 use UnitEnum;
 
-use function get_debug_type;
-use function is_string;
 use function rtrim;
 use function sprintf;
 
-final class DiDefinitionParameterRuntime implements DiDefinitionParameterRuntimeInterface, DiDefinitionNoArgumentsInterface
+final class DiDefinitionParameterRuntime extends DiDefinitionParameterWithContextAbstract implements DiDefinitionParameterRuntimeInterface, DiDefinitionNoArgumentsInterface
 {
-    private ?string $context = null;
     private readonly string $message;
 
     public function __construct(
         private readonly string $name = '',
         ?string $message = null,
     ) {
-        $this->message = $message ?? 'Did you forget to define it?';
-    }
-
-    public function getContext(): ?string
-    {
-        return $this->context;
+        $this->message = $message ?? 'Did you forget to define it? Define parameter using method DiContainerInterface::parameters()->set()';
     }
 
     public function getDefinition(): string
@@ -46,42 +38,14 @@ final class DiDefinitionParameterRuntime implements DiDefinitionParameterRuntime
     {
         $parameterName = $this->nameWithContext($context);
 
-        if ('' === $parameterName) {
-            throw new DiDefinitionException(
-                sprintf('Parameter name must be non-empty string. Parameter name my be set as $context in DiDefinitionParameter::resolve() or in DiDefinitionParameter::setContext(). Current $context type "%s".', get_debug_type($context))
-            );
-        }
-
         if (!$container->parameters()->has($parameterName)) {
             throw new DiDefinitionException(
                 rtrim(
-                    sprintf('The container parameter "%s" must be set in the container at runtime using DiContainerInterface::parameters(). %s', $parameterName, $this->message)
+                    sprintf('The container parameter "%s" must be set in the container at runtime. %s', $parameterName, $this->message)
                 )
             );
         }
 
         return $container->parameters()->get($parameterName);
-    }
-
-    public function setContext(?string $context): static
-    {
-        $this->context = $context;
-
-        return $this;
-    }
-
-    private function nameWithContext(mixed $context): string
-    {
-        if ('' !== $this->name) {
-            return $this->name;
-        }
-
-        if (is_string($this->context) && '' !== $this->context) {
-            return $this->context;
-        }
-
-        return is_string($context) && '' !== $context
-            ? $context
-            : '';
     }
 }
--- src/DiContainer/Interfaces/DiDefinition/DiDefinitionParameterRuntimeInterface.php
+++ src/DiContainer/Interfaces/DiDefinition/DiDefinitionParameterRuntimeInterface.php
@@ -4,49 +4,16 @@ declare(strict_types=1);
 
 namespace Kaspi\DiContainer\Interfaces\DiDefinition;
 
-use Kaspi\DiContainer\Interfaces\DiContainerInterface;
-use Kaspi\DiContainer\Interfaces\Exceptions\DiDefinitionExceptionInterface;
-use Kaspi\DiContainer\Interfaces\Exceptions\ParameterExceptionInterface;
-use Kaspi\DiContainer\Interfaces\Exceptions\ParameterNotFoundExceptionInterface;
-use Kaspi\DiContainer\Interfaces\SourceParametersMutableInterface;
-use UnitEnum;
-
 /**
  * The container parameter must be set in the container at runtime.
  *
  * Any container parameter cannot be defined in configuration files,
  * because value parameter calculate at runtime using container dependencies.
- *
- * @phpstan-import-type SourceParameterType from SourceParametersMutableInterface
  */
-interface DiDefinitionParameterRuntimeInterface extends DiDefinitionInterface
+interface DiDefinitionParameterRuntimeInterface extends DiDefinitionParameterWithContextInterface
 {
     /**
      * Additional message if the container parameter has not been defined yet.
      */
     public function getMessage(): string;
-
-    /**
-     * Parameter name.
-     */
-    public function getDefinition(): string;
-
-    /**
-     * Alternative container parameter name.
-     */
-    public function getContext(): ?string;
-
-    /**
-     * When the parameter name is not defined, $context will provide the container parameter name.
-     */
-    public function setContext(?string $context): static;
-
-    /**
-     * @return SourceParameterType
-     *
-     * @throws ParameterExceptionInterface
-     * @throws ParameterNotFoundExceptionInterface
-     * @throws DiDefinitionExceptionInterface
-     */
-    public function resolve(DiContainerInterface $container, mixed $context = null): array|bool|float|int|string|UnitEnum|null;
 }
