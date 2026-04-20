0a100d265af4498407769383b1e3d52151db7c5e
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-04-20 12:41:49 +0400

    dev: [DiDefinitionParameterRuntimeInterface] Fix description comment.

--- src/DiContainer/Interfaces/DiDefinition/DiDefinitionParameterRuntimeInterface.php
+++ src/DiContainer/Interfaces/DiDefinition/DiDefinitionParameterRuntimeInterface.php
@@ -7,7 +7,7 @@ namespace Kaspi\DiContainer\Interfaces\DiDefinition;
 /**
  * The container parameter must be set in the container at runtime.
  *
- * Any container parameter cannot be defined in configuration files,
+ * Some a container parameter cannot be defined in configuration files,
  * because value parameter calculate at runtime using container dependencies.
  */
 interface DiDefinitionParameterRuntimeInterface extends DiDefinitionParameterWithContextInterface
