7f55aed48ced039ad4787ba48866d3fea6f458cf
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-04-28 14:17:28 +0400

    dev: [Tests] Update integration tests for `DiDefinitionRuntime` in compiled container.

--- tests/Integration/DiDefinitionRuntime/DiDefinitionRuntimeInCompiledContainerTest.php
+++ tests/Integration/DiDefinitionRuntime/DiDefinitionRuntimeInCompiledContainerTest.php
@@ -6,8 +6,11 @@ namespace Tests\Integration\DiDefinitionRuntime;
 
 use Kaspi\DiContainer\DiContainerBuilder;
 use Kaspi\DiContainer\DiContainerConfig;
+use Kaspi\DiContainer\Interfaces\DiContainerBuilderInterface;
+use Kaspi\DiContainer\Interfaces\Exceptions\ContainerBuilderExceptionInterface;
 use Kaspi\DiContainer\Interfaces\Exceptions\DiDefinitionExceptionInterface;
 use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\TestCase;
 use Tests\Integration\DiDefinitionRuntime\Fixtures\Foo;
@@ -22,21 +25,34 @@ use function random_bytes;
 #[CoversNothing]
 class DiDefinitionRuntimeInCompiledContainerTest extends TestCase
 {
-    public function testReplaceRuntimeDefinitionSuccess(): void
-    {
-        vfsStream::setup('root');
-        $containerClass = __NAMESPACE__.'\Container_'.bin2hex(random_bytes(16));
+    private DiContainerBuilderInterface $builder;
+    private vfsStreamDirectory $rootDir;
 
-        $container = (new DiContainerBuilder(
+    protected function setUp(): void
+    {
+        $this->builder = new DiContainerBuilder(
             containerConfig: new DiContainerConfig(
                 useAttribute: true,
             )
-        ))
-            ->import('Tests\\', __DIR__.'/Fixtures')
+        );
+        $this->rootDir = vfsStream::setup('root');
+
+        $containerClass = __NAMESPACE__.'\Container_'.bin2hex(random_bytes(16));
+
+        $this->builder->import('Tests\\', __DIR__.'/Fixtures')
             ->load(__DIR__.'/Fixtures/services.php')
             ->compileToFile(vfsStream::url('root/'), $containerClass, isExclusiveLockFile: false)
-            ->build()
         ;
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->builder, $this->rootDir);
+    }
+
+    public function testReplaceRuntimeDefinitionSuccess(): void
+    {
+        $container = $this->builder->build();
 
         self::assertTrue($container->has('service.foo'));
 
@@ -55,23 +71,37 @@ class DiDefinitionRuntimeInCompiledContainerTest extends TestCase
         $this->expectException(DiDefinitionExceptionInterface::class);
         $this->expectExceptionMessage('The runtime definition with container identifier \'service.foo\' cannot be resolved');
 
-        vfsStream::setup('root');
-        $containerClass = __NAMESPACE__.'\Container_'.bin2hex(random_bytes(16));
-
-        // The definition at container identifier `'service.foo'` not replaced via `$container->set('service.foo', $instance)`
-        $container = (new DiContainerBuilder(
-            containerConfig: new DiContainerConfig(
-                useAttribute: true,
-            )
-        ))
-            ->import('Tests\\', __DIR__.'/Fixtures')
-            ->load(__DIR__.'/Fixtures/services.php')
-            ->compileToFile(vfsStream::url('root/'), $containerClass, isExclusiveLockFile: false)
-            ->build()
-        ;
+        $container = $this->builder->build();
 
         self::assertTrue($container->has('service.foo'));
 
         $container->get(FooAttr::class);
+    }
+
+    public function testDiDefinitionRuntimeOnParameter(): void
+    {
+        $this->expectException(ContainerBuilderExceptionInterface::class);
+        $this->expectExceptionMessage('Cannot compile arguments');
+
+        vfsStream::newFile('redefine_service.php')
+            ->setContent('<?php
+use Tests\Integration\DiDefinitionRuntime\Fixtures\Foo;
+
+return static function (\Kaspi\DiContainer\Interfaces\DefinitionsConfiguratorInterface $configurator) {
+    $configurator->setDefinition(
+        Foo::class,
+        \Kaspi\DiContainer\diAutowire(Foo::class)
+            ->bindArguments(
+                service: \Kaspi\DiContainer\diRuntime("service.foo")
+            )
+    );
+};
+')
+            ->at($this->rootDir)
+        ;
+
+        $this->builder->load(vfsStream::url('root/redefine_service.php'));
+
+        $this->builder->build();
     }
 }
