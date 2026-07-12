27a6400901a4f84ae14aa9ed8ccf7175ab08af08
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-07-12 20:06:46 +0400

    feat: [AttributeReader] Catching an exception when the argument is not a callable object.
    An exception may be thrown when instantiating from the `Autowire` and `DiRuntime` attributes if a non-callable value is passed as the argument for the `$resetter` parameter.

--- tests/AttributeReader/Autowire/AutowireTest.php
+++ tests/AttributeReader/Autowire/AutowireTest.php
@@ -10,6 +10,10 @@ use Kaspi\DiContainer\Interfaces\Exceptions\AutowireExceptionInterface;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
+use Tests\AttributeReader\Autowire\Fixtures\Bar;
+use Tests\AttributeReader\Autowire\Fixtures\BarFailResetter;
+use Tests\AttributeReader\Autowire\Fixtures\Baz;
+use Tests\AttributeReader\Autowire\Fixtures\BazResetter;
 use Tests\AttributeReader\Autowire\Fixtures\ClassWithDiFactoryAndAutowire;
 use Tests\AttributeReader\Autowire\Fixtures\Foo;
 use Tests\AttributeReader\Autowire\Fixtures\MultipleAutowire;
@@ -41,18 +45,21 @@ class AutowireTest extends TestCase
         $this->assertEquals('', $attrs->current()->id);
         $this->assertNull($attrs->current()->isSingleton);
         $this->assertEquals(['foo'], $attrs->current()->arguments);
+        $this->assertFalse($attrs->current()->getResetter());
 
         $attrs->next();
 
         $this->assertEquals('service.singleton', $attrs->current()->id);
         $this->assertTrue($attrs->current()->isSingleton);
         $this->assertEquals(['bar'], $attrs->current()->arguments);
+        $this->assertFalse($attrs->current()->getResetter());
 
         $attrs->next();
 
         $this->assertEquals('service.none_singleton', $attrs->current()->id);
         $this->assertFalse($attrs->current()->isSingleton);
         $this->assertEquals(['baz'], $attrs->current()->arguments);
+        $this->assertFalse($attrs->current()->getResetter());
 
         $attrs->next();
 
@@ -75,5 +82,28 @@ class AutowireTest extends TestCase
         $this->expectExceptionMessageMatches('/The attributes .+Autowire and .+DiRuntime cannot be declared together/');
 
         [...AttributeReader::getAutowireAttribute(new ReflectionClass(Foo::class))];
+    }
+
+    public function testAutowireWithFailResetter(): void
+    {
+        $this->expectException(AutowireExceptionInterface::class);
+        $this->expectExceptionMessageMatches('/Argument.+\(\$resetter\) must be of type callable\|string\|false, array given/');
+
+        [...AttributeReader::getAutowireAttribute(new ReflectionClass(BarFailResetter::class))];
+    }
+
+    public function testAutowireResetterAsMethod(): void
+    {
+        $autowire = AttributeReader::getAutowireAttribute(new ReflectionClass(Bar::class))->current();
+
+        self::assertEquals('doReset', $autowire->getResetter());
+    }
+
+    public function testAutowireResetterAsCallable(): void
+    {
+        $autowire = AttributeReader::getAutowireAttribute(new ReflectionClass(Baz::class))->current();
+
+        self::assertIsCallable($resetter = $autowire->getResetter());
+        self::assertEquals([BazResetter::class, 'doReset'], $resetter);
     }
 }
