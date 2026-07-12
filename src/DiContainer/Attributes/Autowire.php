f3c86b444a118cf755ba5ed6cedd4da1868cf67c
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-07-12 16:43:49 +0400

    feat: [Autowire] The `\Kaspi\DiContainer\Attributes\Autowire::getResetter()` method provides a resetter for the container definition.

--- src/DiContainer/Attributes/Autowire.php
+++ src/DiContainer/Attributes/Autowire.php
@@ -14,9 +14,15 @@ use Kaspi\DiContainer\Interfaces\DiDefinition\DiDefinitionArgumentsInterface;
 final class Autowire
 {
     /**
+     * @var callable|false|non-empty-string
+     */
+    private $resetter;
+
+    /**
      * @param array<non-empty-string|non-negative-int, DiDefinitionType|mixed>              $arguments arguments for `__constructor()` method
      * @param null|list<Tag>|Tag                                                            $tags      tags bound to the current `Autowire` attribute
      * @param null|array<non-empty-string, list<Setup|SetupImmutable>|Setup|SetupImmutable> $setups    methods for configuring service in current `Autowire` attribute
+     * @param callable(object $object): void|false|non-empty-string                         $resetter  provides a reset mechanism for an object obtained via the container's `get()` method
      */
     public function __construct(
         public readonly string $id = '',
@@ -24,5 +30,18 @@ final class Autowire
         public readonly array $arguments = [],
         public readonly array|Tag|null $tags = null,
         public readonly ?array $setups = null,
-    ) {}
+        callable|false|string $resetter = false,
+    ) {
+        $this->resetter = $resetter;
+    }
+
+    /**
+     * Provides a reset mechanism for an object obtained via the container's `get()` method.
+     *
+     * @return callable(object $object): void|false|non-empty-string
+     */
+    public function getResetter(): callable|false|string
+    {
+        return $this->resetter;
+    }
 }
