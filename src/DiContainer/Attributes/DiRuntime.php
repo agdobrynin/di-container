ed77bc19e3288538e4265794bbd115fc62c14545
Alexey Dobrynin <ag.dobrynin@gmail.com>
2026-07-12 16:46:29 +0400

    feat: [DiRuntime] The `\Kaspi\DiContainer\Attributes\DiRuntime::getResetter()` method provides a resetter for the container definition.

--- src/DiContainer/Attributes/DiRuntime.php
+++ src/DiContainer/Attributes/DiRuntime.php
@@ -10,12 +10,31 @@ use Attribute;
 final class DiRuntime
 {
     /**
-     * @param class-string|string $containerIdentifier
-     * @param null|list<Tag>|Tag  $tags                tags bound to the current `DiRuntime` attribute
+     * @var callable|false|non-empty-string
+     */
+    private $resetter;
+
+    /**
+     * @param class-string|string                                   $containerIdentifier
+     * @param null|list<Tag>|Tag                                    $tags                tags bound to the current `DiRuntime` attribute
+     * @param callable(object $object): void|false|non-empty-string $resetter            provides a reset mechanism for an object obtained via the container's `get()` method
      */
     public function __construct(
         public readonly string $containerIdentifier = '',
         public readonly ?string $message = null,
         public readonly array|Tag|null $tags = null,
-    ) {}
+        callable|false|string $resetter = false,
+    ) {
+        $this->resetter = $resetter;
+    }
+
+    /**
+     * Provides a reset mechanism for an object obtained via the container's `get()` method.
+     *
+     * @return callable(object $object): void|false|non-empty-string
+     */
+    public function getResetter(): callable|false|string
+    {
+        return $this->resetter;
+    }
 }
